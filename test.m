957f085247777d0fa606d11570a583353c02a100
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-05-30 15:09:28 +0200

    hexagonal grid done

--- test.m
+++ test.m
@@ -1,11 +1,16 @@
 %Created by Pablo Vicente-Munuera
 
-Rad3Over2 = sqrt(3) / 2;
-[X Y] = meshgrid(0:1:41);
+clf
+rgb = imread('Datos\Data\Casos\CaSO 1. Y01_01B16459B\CoreA\1. Y01_01B16459A 015 RET.tif');
+sizeRgb = size(rgb) + size(rgb)/8
+imshow(rgb)
+hold on
+Rad3Over2 = (sqrt(3) / 2);
+hexagonDiameter = 40;
+[X Y] = meshgrid(0:hexagonDiameter:sizeRgb(1));
 n = size(X,1);
 X = Rad3Over2 * X;
-Y = Y + repmat([0 0.5],[n,n/2]);
+Y = Y + repmat([0 hexagonDiameter/2],[n, n/2]);
 
 % Plot the hexagonal mesh, including cell borders
-[XV YV] = voronoi(X(:),Y(:)); plot(XV,YV,'b-')
-axis equal, axis([0 20 0 20]), zoom on
+voronoi(X(:),Y(:));
