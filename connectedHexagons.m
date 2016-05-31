f869664459e33feae602f91fca7ffe05ba31b3c8
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-05-31 13:53:32 +0200

    create network init

---
+++ connectedHexagons.m
@@ -0,0 +1,12 @@
+function [ v ] = connectedHexagons( Img, i, j )
+%CONNECTEDHEXAGONS Summary of this function goes here
+%   Detailed explanation goes here
+    v = [];
+    for i1 = i-1:i+1
+       for j1 = j-1:j+1
+          v = [v;Img(i1,j1)]; 
+       end
+    end
+    v(v==0) = [];
+end
+
---
+++ createNetwork.m
@@ -0,0 +1,25 @@
+%Developed by Pablo Vicente-Munuera
+
+Img=imread('Datos\Data\Casos\CASO 1. Y01_01B16459B\CoreA\2. Y01_01B16459A 015 COL.tif');
+mask = importdata('Mascaras\HexagonalMask20Diamet.mat');
+
+v1 = [];
+v2 = [];
+
+for i = 2:size(Img,1)
+    for j = 2:size(Img,2)
+        if (mask(i,j) ==  0 && Img(i,j) ~= 255)
+            %Add to verteces list
+            [vSides] = connectedHexagons(Img, i, j);
+            %%Add it to the vertex list
+                v1 = [v1;vSides(1)];
+                v2 = [v2;vSides(2)];
+            if(size(vSides) == 3)
+                v1 = [v1;vSides(1)];
+                v2 = [v2;vSides(3)];
+                v1 = [v1;vSides(2)];
+                v2 = [v2;vSides(3)];
+            end
+        end
+    end
+end
