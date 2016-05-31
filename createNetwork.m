aa73798dd5b2d0d709bbd4c5a4c5ee043587fe5f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-05-31 15:05:50 +0200

    for differents files

--- createNetwork.m
+++ createNetwork.m
@@ -1,34 +1,43 @@
 %Developed by Pablo Vicente-Munuera
 
-Img=imread('Datos\Data\Casos\CASO 1. Y01_01B16459B\CoreA\2. Y01_01B16459A 015 COL.tif');
-mask = importdata('Mascaras\HexagonalMask20Diamet.mat');
+[stat,struc] = fileattrib;
+PathCurrent = struc.Name;
 
-v1 = [];
-v2 = [];
-
-for i = 2:size(Img,1)
-    for j = 2:size(Img,2)
-        if (mask(i,j) ==  0 && Img(i,j) == 255)
-            %Add to verteces list
-            [vSides] = connectedHexagons(mask, i, j);
-            %%Add it to the vertex list
+lee_imagenes = dir(PathCurrent);
+lee_imagenes = lee_imagenes(3:size(lee_imagenes,1))
+for imK = 1:size(lee_imagenes,1)
+    lee_imagenes(imK)
+    Img=imread(lee_imagenes(imK).name);
+    mask = importdata('..\..\..\..\..\Mascaras\HexagonalMask20Diamet.mat');
+    
+    v1 = [];
+    v2 = [];
+    
+    for i = 2:size(Img,1)
+        for j = 2:size(Img,2)
+            if (mask(i,j) ==  0 && Img(i,j) == 255)
+                %Add to verteces list
+                [vSides] = connectedHexagons(mask, i, j);
+                %%Add it to the vertex list
                 v1 = [v1;vSides(1)];
                 v2 = [v2;vSides(2)];
-            if(size(vSides) == 3)
-                v1 = [v1;vSides(1)];
-                v2 = [v2;vSides(3)];
-                v1 = [v1;vSides(2)];
-                v2 = [v2;vSides(3)];
+                if(size(vSides) == 3)
+                    v1 = [v1;vSides(1)];
+                    v2 = [v2;vSides(3)];
+                    v1 = [v1;vSides(2)];
+                    v2 = [v2;vSides(3)];
+                end
             end
         end
     end
-end
-
-classes = unique(v1+v2);
-%Creating the Incidence matrix
-incidenceMatrix = zeros(size(classes,1), size(classes,1));
-
-for i = 1:size(v1,1)
-    v2Index = classes(classes == v2(i));
-    incidenceMatrix(i, v2Index) = 1;
+    
+    classes = unique(v1+v2);
+    %Creating the Incidence matrix
+    incidenceMatrix = zeros(size(classes,1), size(classes,1));
+    
+    %parpool(6);
+    for i = 1:size(v1,1)
+        v2Index = classes(classes == v2(i));
+        incidenceMatrix(i, v2Index) = 1;
+    end
 end
