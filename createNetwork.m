8282ba040b53354f2979771a7169ff2c79ee8e1f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-01 10:48:19 +0200

    changed name of matrix and pixeles not black

--- createNetwork.m
+++ createNetwork.m
@@ -1,10 +1,10 @@
 %Developed by Pablo Vicente-Munuera
 
-function [ ] = createIncidenceMatrix()
+function [ ] = createNetwork()
     [stat,struc] = fileattrib;
     PathCurrent = struc.Name;
     lee_imagenes = dir(PathCurrent);
-    lee_imagenes = lee_imagenes(3:size(lee_imagenes,1))
+    lee_imagenes = lee_imagenes(3:size(lee_imagenes,1)-1)
     for imK = 1:size(lee_imagenes,1)
         lee_imagenes(imK).name
         Img=imread(lee_imagenes(imK).name);
@@ -15,7 +15,7 @@ function [ ] = createIncidenceMatrix()
 
         for i = 2:size(Img,1)
             for j = 2:size(Img,2)
-                if (mask(i,j) ==  0 && Img(i,j) == 255)
+                if (mask(i,j) ==  0 && Img(i,j) ~= 0)
                     %Add to verteces list
                     [vSides] = connectedHexagons(mask, i, j);
                     %%Add it to the vertex list
@@ -33,17 +33,18 @@ function [ ] = createIncidenceMatrix()
 
         classes = unique([v1,v2]);
         %Creating the Incidence matrix
-        incidenceMatrix = zeros(size(classes,1), size(classes,1));
+        adjacencyMatrix = zeros(size(classes,1), size(classes,1));
 
         for i = 1:size(classes,1)
             v2Index = find(classes == v2(i));
-            incidenceMatrix(i, v2Index) = incidenceMatrix(i, v2Index) + 1;
-            incidenceMatrix(v2Index, i) = incidenceMatrix(v2Index, i) + 1;
+            adjacencyMatrix(i, v2Index) = adjacencyMatrix(i, v2Index) + 1;
+            adjacencyMatrix(v2Index, i) = adjacencyMatrix(v2Index, i) + 1;
         end
 
+        bg = biograph(adjacencyMatrix,[],'ShowArrows','off','ShowWeights','off');
         
         inNameFile = strsplit(strrep(lee_imagenes(imK).name,' ','_'), '.')
-        outputFileName = strcat('Incidences\incidenceMatrix', inNameFile(1), 'hexagonalMask20Diamet.mat')
-        save(outputFileName{:},'incidenceMatrix', '-v7.3');
+        outputFileName = strcat('Adjacency\adjacencyMatrix', inNameFile(1), 'hexagonalMask20Diamet.mat')
+        save(outputFileName{:}, 'adjacencyMatrix', 'bg', '-v7.3');
     end
 end
