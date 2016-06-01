7cb94de6b4898f7d0c0a4ec72e93615d778ef173
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-01 15:21:05 +0200

    init GetCentroidOfClass

---
+++ GetCentroidOfClass.m
@@ -0,0 +1,9 @@
+function [ vCentroids, vCluster ] = GetCentroidOfClass( mask, C, S)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    for i = 1:S
+       indexesOfClusters = find(C==i);
+       
+    end
+end
+
