88ddbaff93459b277ebc2301c32cda091d463f62
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-01 15:35:02 +0200

    no outputs

--- GetCentroidOfCluster.m
+++ GetCentroidOfCluster.m
@@ -1,4 +1,4 @@
-function [ vCentroids ] = GetCentroidOfCluster( mask, C, S)
+function [ vCentroids ] = GetCentroidOfCluster(mask, C, S)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     vCentroids = zeros(S);
@@ -7,7 +7,7 @@ function [ vCentroids ] = GetCentroidOfCluster( mask, C, S)
        rows = [];
        cols = [];
        for index = indexesOfClusters
-           [row,col] = find(mask == index)
+           [row,col] = find(mask == index);
            rows = [rows; row];
            cols = [cols; col];
        end
