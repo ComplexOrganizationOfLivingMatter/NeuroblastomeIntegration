869f138bfcf6c53bd71f6095a7907380d2310a4e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-01 11:49:28 +0200

    mean of cells inside voronoi hexagons as weights

--- createNetwork.m
+++ createNetwork.m
@@ -12,10 +12,18 @@ function [ ] = createNetwork()
 
         v1 = [];
         v2 = [];
+        classesArea = containers.Map('KeyType','int32','ValueType','int32');
 
         for i = 2:size(Img,1)
             for j = 2:size(Img,2)
-                if (mask(i,j) ==  0 && Img(i,j) ~= 0)
+                %If trasspass the the boundary we put an edge
+                if (mask(i,j) ~= 0 && Img(i,j) ~= 0)
+                    if classesArea.isKey(mask(i,j))
+                        classesArea(mask(i,j)) = classesArea(mask(i,j)) + 1;
+                    else
+                        classesArea(mask(i,j)) = 1;
+                    end
+                elseif (mask(i,j) ==  0 && Img(i,j) ~= 0)
                     %Add to verteces list
                     [vSides] = connectedHexagons(mask, i, j);
                     %%Add it to the vertex list
@@ -37,14 +45,23 @@ function [ ] = createNetwork()
 
         for i = 1:size(classes,1)
             v2Index = find(classes == v2(i));
-            adjacencyMatrix(i, v2Index) = adjacencyMatrix(i, v2Index) + 1;
-            adjacencyMatrix(v2Index, i) = adjacencyMatrix(v2Index, i) + 1;
+            v1Area = 0;
+            v2Area = 0;
+            if classesArea.isKey(v1(i))
+                v1Area = classesArea(v1(i));
+            end
+            if classesArea.isKey(v2(v2Index))
+               v2Area = classesArea(v2(v2Index)); 
+            end
+            adjacencyMatrix(i, v2Index) = (v1Area + v2Area)/2;
+            adjacencyMatrix(v2Index, i) = (v1Area + v2Area)/2;
         end
 
-        bg = biograph(adjacencyMatrix,[],'ShowArrows','off','ShowWeights','off');
+        bg = biograph(adjacencyMatrix,[],'ShowArrows','off','ShowWeights','on');
         
         inNameFile = strsplit(strrep(lee_imagenes(imK).name,' ','_'), '.')
         outputFileName = strcat('Adjacency\adjacencyMatrix', inNameFile(1), 'hexagonalMask20Diamet.mat')
         save(outputFileName{:}, 'adjacencyMatrix', 'bg', '-v7.3');
+        break;
     end
 end
