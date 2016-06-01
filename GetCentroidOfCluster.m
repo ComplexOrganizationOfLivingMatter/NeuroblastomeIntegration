2406d023c11fdc00f18108006f66bf83d85194e4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-01 17:04:06 +0200

    two vectos instead of matrices

--- GetCentroidOfCluster.m
+++ GetCentroidOfCluster.m
@@ -1,8 +1,8 @@
 function [ vCentroidsRows, vCentroidsCols ] = GetCentroidOfCluster(mask, C, S)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    vCentroidsRows = zeros(S);
-    vCentroidsCols = zeros(S);
+    vCentroidsRows = zeros(1,S);
+    vCentroidsCols = zeros(1,S);
     %parpool(4);
     parfor i = 1:S
        indexesOfClusters = find(C==i);
@@ -13,8 +13,8 @@ function [ vCentroidsRows, vCentroidsCols ] = GetCentroidOfCluster(mask, C, S)
            rows = [rows; row];
            cols = [cols; col];
        end
-       vCentroidsRows = [vCentroidsRows; mean(rows)];
-       vCentroidsCols = [vCentroidsCols; mean(col)];
+       vCentroidsRows(i) = mean(rows);
+       vCentroidsCols(i) = mean(col);
     end
 end
 
