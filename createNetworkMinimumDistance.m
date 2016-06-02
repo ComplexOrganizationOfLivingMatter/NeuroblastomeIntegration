32f9a3784deaa83cbfe753ad5881c7e996893d2b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-02 11:54:49 +0200

    creating network minimum distance with all files

---
+++ createNetworkMinimumDistance.m
@@ -0,0 +1,19 @@
+function [ ] = createNetworkMinimumDistance( )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    [stat,struc] = fileattrib;
+    PathCurrent = struc.Name;
+    lee_imagenes = dir(PathCurrent);
+    lee_imagenes = lee_imagenes(3:size(lee_imagenes,1)-1)
+    for imK = 1:size(lee_imagenes,1)
+        lee_imagenes(imK).name
+        Img=imread(lee_imagenes(imK).name);
+        adjacencyMatrix = minimumDistanceBetweenNeighbourGraph(Img);
+        
+        %Saving file
+        inNameFile = strsplit(strrep(lee_imagenes(imK).name,' ','_'), '.');
+        outputFileName = strcat('Adjacency\minimumDistanceClasses', inNameFile(1), '.mat')
+        save(outputFileName{:}, 'adjacencyMatrix', '-v7.3');
+    end
+end
+
