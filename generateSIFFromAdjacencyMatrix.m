3909d495b4a8601ec58bd65dd64f8a506c264383
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-02 14:50:04 +0200

    export to cytoscape

---
+++ generateSIFFromAdjacencyMatrix.m
@@ -0,0 +1,16 @@
+function [ ] = generateSIFFromAdjacencyMatrix( adjacencyMatrix, nameFile )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    fileID = fopen(nameFile,'w');
+    adjacencyMatrix = full(adjacencyMatrix);
+    for i = 1:size(adjacencyMatrix,1)
+        for j = 1:size(adjacencyMatrix,2)
+            if adjacencyMatrix(i,j) > 0
+                fprintf(fileID, '%d %d %d\n', i,j, adjacencyMatrix(i,j));
+            end
+        end
+    end
+    fclose(fileID);
+
+end
+
