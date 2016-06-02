308c9815bd58da9f95787a77cb88cd934db70428
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-02 12:50:16 +0200

    debuggind done, i think it works

--- GetCompleteGraphWithMinimumDistances.m
+++ GetCompleteGraphWithMinimumDistances.m
@@ -3,18 +3,24 @@ function [ adjacencyMatrix ] = GetCompleteGraphWithMinimumDistances( distanceBet
 %   Detailed explanation goes here
 
     mDistanceBetweenClusters = squareform(distanceBetweenClusters);
-    mDistanceBetweenClusters(logical(eye(size(mDistanceBetweenClusters)))) = intmax('int8');
+    mDistanceBetweenClusters(logical(eye(size(mDistanceBetweenClusters)))) = intmax('int16');
 
-    adjacencyMatrix = sparse(adjacencyMatrix);
-    while graphconncomp(adjacencyMatrix) > 1
-        [rowMin, colMin] = find(mDistanceBetweenClusters == min(mDistanceBetweenClusters(:)), 1);
-        class1 = find(C == rowMin, 1);
-        class2 = find(C == colMin, 1);
-        adjacencyMatrix(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
-        adjacencyMatrix(class2, class1) = mDistanceBetweenClusters(rowMin, colMin);
+    while graphconncomp(adjacencyMatrix, 'Directed', 'false') > 1
+        minimumDistance = min(mDistanceBetweenClusters(:));
+        [rowMin, colMin] = find(mDistanceBetweenClusters == minimumDistance, 1);
+        if size(C,1) > 1
+           class1 = find(C == rowMin, 1);
+            class2 = find(C == colMin, 1);
+            adjacencyMatrix(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrix(class2, class1) = mDistanceBetweenClusters(rowMin, colMin);
+        else
+            adjacencyMatrix(rowMin, colMin) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrix(colMin, rowMin) = mDistanceBetweenClusters(rowMin, colMin);
+        end
         
-        mDistanceBetweenClusters(rowMin, colMin) = intmax('int32');
-        mDistanceBetweenClusters(colMin, rowMin) = intmax('int32');
+        
+        mDistanceBetweenClusters(rowMin, colMin) = intmax('int16');
+        mDistanceBetweenClusters(colMin, rowMin) = intmax('int16');
     end
 end
 
