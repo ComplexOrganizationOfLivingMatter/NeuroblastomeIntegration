21bae76f44c10d44d961a351e158b0499e618a16
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-03 14:10:08 +0200

    init every distance

---
+++ GetCompleteGraphWithEveryDistance.m
@@ -0,0 +1,26 @@
+function [ adjacencyMatrix ] = GetCompleteGraphWithMinimumDistances( distanceBetweenClusters, adjacencyMatrix, C)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+    mDistanceBetweenClusters = squareform(distanceBetweenClusters);
+    mDistanceBetweenClusters(logical(eye(size(mDistanceBetweenClusters)))) = intmax('int32');
+
+    while graphconncomp(adjacencyMatrix, 'Directed', 'false') > 1
+        minimumDistance = min(mDistanceBetweenClusters(:));
+        [rowMin, colMin] = find(mDistanceBetweenClusters == minimumDistance, 1);
+        if size(C,1) > 1
+           class1 = find(C == rowMin, 1);
+            class2 = find(C == colMin, 1);
+            adjacencyMatrix(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrix(class2, class1) = mDistanceBetweenClusters(rowMin, colMin);
+        else
+            adjacencyMatrix(rowMin, colMin) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrix(colMin, rowMin) = mDistanceBetweenClusters(rowMin, colMin);
+        end
+        
+        
+        mDistanceBetweenClusters(rowMin, colMin) = intmax('int32');
+        mDistanceBetweenClusters(colMin, rowMin) = intmax('int32');
+    end
+end
+
