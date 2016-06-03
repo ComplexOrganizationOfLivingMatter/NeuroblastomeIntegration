fb924ac72f1ae0f9f42ef0852552513d74573431
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-03 14:13:48 +0200

    all the distance, complete graph

--- GetCompleteGraphWithEveryDistance.m
+++ GetCompleteGraphWithEveryDistance.m
@@ -5,22 +5,19 @@ function [ adjacencyMatrix ] = GetCompleteGraphWithMinimumDistances( distanceBet
     mDistanceBetweenClusters = squareform(distanceBetweenClusters);
     mDistanceBetweenClusters(logical(eye(size(mDistanceBetweenClusters)))) = intmax('int32');
 
-    while graphconncomp(adjacencyMatrix, 'Directed', 'false') > 1
-        minimumDistance = min(mDistanceBetweenClusters(:));
-        [rowMin, colMin] = find(mDistanceBetweenClusters == minimumDistance, 1);
-        if size(C,1) > 1
-           class1 = find(C == rowMin, 1);
-            class2 = find(C == colMin, 1);
-            adjacencyMatrix(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
-            adjacencyMatrix(class2, class1) = mDistanceBetweenClusters(rowMin, colMin);
-        else
-            adjacencyMatrix(rowMin, colMin) = mDistanceBetweenClusters(rowMin, colMin);
-            adjacencyMatrix(colMin, rowMin) = mDistanceBetweenClusters(rowMin, colMin);
-        end
-        
-        
-        mDistanceBetweenClusters(rowMin, colMin) = intmax('int32');
-        mDistanceBetweenClusters(colMin, rowMin) = intmax('int32');
+    for i = 1:size(mDistanceBetweenClusters,1)
+		for j = 1:size(mDistanceBetweenClusters,2)
+			[rowMin, colMin] = [i, j]
+			if size(C,1) > 1
+			   class1 = find(C == rowMin, 1);
+			   class2 = find(C == colMin, 1);
+			   adjacencyMatrix(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
+			   adjacencyMatrix(class2, class1) = mDistanceBetweenClusters(rowMin, colMin);
+			else
+				adjacencyMatrix(rowMin, colMin) = mDistanceBetweenClusters(rowMin, colMin);
+				adjacencyMatrix(colMin, rowMin) = mDistanceBetweenClusters(rowMin, colMin);
+			end
+		end
     end
 end
 
