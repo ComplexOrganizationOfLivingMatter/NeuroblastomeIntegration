03531eb4b2e3ab248ea1b768f8870bc282b510f4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-03 10:02:17 +0200

    no negative images

--- createNetworkMinimumDistance.m
+++ createNetworkMinimumDistance.m
@@ -6,7 +6,7 @@ function [ ] = createNetworkMinimumDistance( )
     lee_imagenes = dir(PathCurrent);
     lee_imagenes = lee_imagenes(3:size(lee_imagenes,1))
     for imK = 1:size(lee_imagenes,1)
-        if (lee_imagenes(imK).isdir == 0 && size(strfind(lee_imagenes(imK).name, 'negativ'),1) == 0)
+        if (lee_imagenes(imK).isdir == 0 && size(strfind(lower(lee_imagenes(imK).name), 'neg'),1) == 0)
             lee_imagenes(imK).name
             Img=imread(lee_imagenes(imK).name);
             adjacencyMatrix = minimumDistanceBetweenNeighbourGraph(Img);
