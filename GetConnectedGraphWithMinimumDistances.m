2195d098e88192ad0746603973aff5ee34a6373f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-03 16:57:28 +0200

    bug!!!

--- GetConnectedGraphWithMinimumDistances.m
+++ GetConnectedGraphWithMinimumDistances.m
@@ -8,7 +8,7 @@ function [ adjacencyMatrix ] = GetConnectedGraphWithMinimumDistances( distanceBe
     while graphconncomp(adjacencyMatrix, 'Directed', 'false') > 1
         minimumDistance = min(mDistanceBetweenClusters(:));
         [rowMin, colMin] = find(mDistanceBetweenClusters == minimumDistance, 1);
-        if size(C,1) > 1
+        if size(C,2) > 1
            class1 = find(C == rowMin, 1);
             class2 = find(C == colMin, 1);
             adjacencyMatrix(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
--- createNetworkHexagonalGridSharedSide.m
+++ createNetworkHexagonalGridSharedSide.m
@@ -4,9 +4,9 @@ function [ ] = createNetworkHexagonalGridSharedSide()
     [stat,struc] = fileattrib;
     PathCurrent = struc.Name;
     lee_imagenes = dir(PathCurrent);
-    lee_imagenes = lee_imagenes(3:size(lee_imagenes,1))
+    lee_imagenes = lee_imagenes(4:size(lee_imagenes,1))
     for imK = 1:size(lee_imagenes,1)
-        if (lee_imagenes(imK).isdir == 0 && size(strfind(lee_imagenes(imK).name, 'negativ'),1) == 0)
+        if (lee_imagenes(imK).isdir == 0 && size(strfind(lower(lee_imagenes(imK).name), 'neg'),1) == 0)
             lee_imagenes(imK).name
             Img=imread(lee_imagenes(imK).name);
             Img = im2bw(Img, 0.2);
