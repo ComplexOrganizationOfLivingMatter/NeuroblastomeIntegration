b447afe057be229520c61d7e1d824f393c1e0cef
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-06 15:46:38 +0200

    only 1 fi

--- createNetworkHexagonalGridSharedSide.m
+++ createNetworkHexagonalGridSharedSide.m
@@ -10,7 +10,7 @@ function [ ] = createNetworkHexagonalGridSharedSide()
             lee_imagenes(imK).name
             Img=imread(lee_imagenes(imK).name);
             Img = im2bw(Img, 0.2);
-            for numMask = 2:50
+            for numMask = 20:20
                 inNameFile = strsplit(strrep(lee_imagenes(imK).name,' ','_'), '.');
                 outputFileName = strcat('Adjacency\adjacencyMatrix', inNameFile(1), 'hexagonalSharedSideMask', num2str(numMask),'Diamet.mat')
 				outputFileNameSif = strcat('visualize\adjacencyMatrix', inNameFile(1), 'hexagonalSharedSideMask', num2str(numMask),'Diamet.cvs');
