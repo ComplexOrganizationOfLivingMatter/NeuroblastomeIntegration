435c2655b85db328c199d3fb7882d0cf0aed9ed2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-07 17:04:02 +0200

    staring again

--- createNetworkContigousHexagonalGridMeanArea.m
+++ createNetworkContigousHexagonalGridMeanArea.m
@@ -18,64 +18,14 @@ function [ ] = createNetworkContigousHexagonalGridMeanArea()
                     maskName = strcat('..\..\..\..\..\Mascaras\HexagonalMask', num2str(numMask), 'Diamet.mat');
                     mask = importdata(maskName);
 
-                    v1 = [];
-                    v2 = [];
-                    classesArea = containers.Map('KeyType','single','ValueType','single');
-                    hexagonArea = containers.Map('KeyType','single','ValueType','single');
-
-                    for i = 2:size(Img,1)
-                        for j = 2:size(Img,2)
-
-                            if (mask(i,j) ~= 0) %Area of the hexagon itself
-                                if hexagonArea.isKey(mask(i,j))
-                                    hexagonArea(mask(i,j)) = hexagonArea(mask(i,j)) + 1;
-                                else
-                                    hexagonArea(mask(i,j)) = 1;
-                                end
-                                if (Img(i,j) ~= 0) %Area of cells/fibers in the hexagon
-                                    if classesArea.isKey(mask(i,j))
-                                        classesArea(mask(i,j)) = classesArea(mask(i,j)) + 1;
-                                    else
-                                        classesArea(mask(i,j)) = 1;
-                                    end
-                                end
-                            elseif (mask(i,j) ==  0 && Img(i,j) ~= 0) %If trasspass the boundary we put an edge
-                                %Add to verteces list
-                                [vSides] = connectedHexagons(mask, i, j);
-                                %%Add it to the vertex list
-                                v1 = [v1;vSides(1)];
-                                v2 = [v2;vSides(2)];
-                                if(size(vSides) == 3)
-                                    v1 = [v1;vSides(1)];
-                                    v2 = [v2;vSides(3)];
-                                    v1 = [v1;vSides(2)];
-                                    v2 = [v2;vSides(3)];
-                                end
-                            end
-                        end
-                    end
-
-                    classes = unique([v1,v2]);
-                    %Creating the Incidence matrix
-                    %adjacencyMatrix = zeros(size(classes,1), size(classes,1));
-                    adjacencyMatrix = sparse(size(classes,1), size(classes,1));
-
-                    for i = 1:size(v1,1)
-                        v1Index = find(classes == v1(i));
-                        v2Index = find(classes == v2(i));
-                        v1Area = 0;
-                        v2Area = 0;
-                        if classesArea.isKey(v1(v1Index))
-                            v1Area = classesArea(v1(v1Index))/hexagonArea(v1(v1Index));
-                        end
-                        if classesArea.isKey(v2(v2Index))
-                            v2Area = classesArea(v2(v2Index))/hexagonArea(v2(v2Index));
-                        end
-                        adjacencyMatrix(v1Index, v2Index) = (v1Area + v2Area)/2;
-                        adjacencyMatrix(v2Index, v1Index) = (v1Area + v2Area)/2;
+                    adjacencyMatrixHexagons = zeros(mask(max(size(Img)), max(size(Img))))
+                    if size(adjacencyMatrixHexagons,1)==0
+                        
                     end
                     
-                    adjacencyMatrix = adjacencyMatrix / max(adjacencyMatrix(:));
+                    for i=1: mask(6999,6999) %Going through the classes
+                       
+                    end
 
 
                     %clear v1 v2 classesArea mask
