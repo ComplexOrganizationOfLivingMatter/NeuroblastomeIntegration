1ca9ff35f975f46a6452027702e86aed78cb250a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-08 16:18:16 +0200

    exporting to csv

--- calculateBasicMeasuresOfAllNetworksOfDirectory.m
+++ calculateBasicMeasuresOfAllNetworksOfDirectory.m
@@ -11,13 +11,16 @@ function [ ] = calculateBasicMeasuresOfAllNetworksOfDirectory( )
          inNameFile = strsplit(strrep(lee_imagenes(imK).name,' ','_'), '.');
          outputFileName = strcat('..\..\stats', inNameFile(1), '.csv')
          if exist(outputFileName{:}, 'file') ~= 2
+            fileID = fopen(outputFileName, 'w');
+            fprintf('Type, Clustering Coefficient UU, Clustering Coefficient WU, Assortivity, Density UU, Efficiency, Diameter, Characteristic path length\r\n');
             load(lee_imagenes(imK).name);
             if exist('adjacencyMatrix', 'var') == 1
-                calculateBasicMeasuresOfNetwork(adjacencyMatrix);
+                fprintf(fileID, 'Clusters, %s\r\n', calculateBasicMeasuresOfNetwork(adjacencyMatrix));
             end
             if exist('adjacencyMatrixComplete', 'var') == 1
-                calculateBasicMeasuresOfNetwork(adjacencyMatrixComplete);
+                fprintf(fileID, 'Connected, %s\r\n', calculateBasicMeasuresOfNetwork(adjacencyMatrixComplete));
             end
+            fclose(fileID);
          end
     end
 
