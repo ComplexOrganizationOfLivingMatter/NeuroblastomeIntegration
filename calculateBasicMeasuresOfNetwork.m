199d0e27b2cf96b99c414a18987f73f237835331
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-08 15:37:56 +0200

    implemmented

--- calculateBasicMeasuresOfNetwork.m
+++ calculateBasicMeasuresOfNetwork.m
@@ -1,10 +1,27 @@
-function [ ] = calculateBasicMeasuresOfNetwork( adjacencyMatrixComplete )
-%calculateBasicMeasuresOfNetwork Summary of this function goes here
+function [ ] = calculateBasicMeasuresOfNetwork( adjacencyMatrix )
+%calculateBasicMeasuresOfNetwork Calculate several important measures of a given network
 %   This file should calculate the following measures of a network:
 %   Clustering coefficient, network diameter, connectedness,
 %   network density, average path length (or characteristic path length),
-%   average degree.
-
+%   average degree and others.
+	
+	%Clustering coefficient unweighted undirected
+	ccuu = clustering_coef_bu(adjacencyMatrix);
+	%Clustering coefficient weighted undirected
+	ccwu = clustering_coef_wu(adjacencyMatrix);
+	
+	%Assortivity
+	assor = assortativity(adjacencyMatrix, 0);
+	
+	%Density undirected unweighted
+	den = density_ud(adjacencyMatrix);
+	
+	%   Outputs:    lambda,         characteristic path length
+	%               efficiency,     global efficiency
+	%               ecc,            eccentricity (for each vertex)
+	%               radius,         radius of graph
+	%               diameter,       diameter of graph
+	[lambda,efficiency,ecc,radius,diameter] = charpath(adjacencyMatrix);
 
 end
 
