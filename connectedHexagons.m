512a54f6e992184ef7b6f2f8abd23aab4cc2f9c0
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-14 14:28:06 +0200

    check boundaries

--- connectedHexagons.m
+++ connectedHexagons.m
@@ -2,9 +2,13 @@ function [ v ] = connectedHexagons( Img, i, j )
 %CONNECTEDHEXAGONS Summary of this function goes here
 %   Detailed explanation goes here
     v = [];
-    for i1 = i-1:i+1
-       for j1 = j-1:j+1
-          v = [v;Img(i1,j1)]; 
+    for i1 = i-1:i+1 
+       if i1 > 0 && i1 < size(Img, 1)
+           for j1 = j-1:j+1
+               if j1 > 0 && j1 < size(Img, 2)
+                v = [v;Img(i1,j1)];
+               end
+           end
        end
     end
     v = unique(v);
