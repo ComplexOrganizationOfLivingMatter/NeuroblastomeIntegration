04669ece1e86d490a0f8f8e580ddb87db48ff561
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-15 11:26:44 +0200

    done

--- createCSVFromStats.py
+++ createCSVFromStats.py
@@ -1,9 +1,11 @@
 #Developed by Pablo Vicente-Munuera
 import os
 from os import path
+import re
 
 directory_path = 'E:/Pablo/Neuroblastoma/Datos/Data/Casos'
 directories = [x for x in os.listdir(directory_path) if path.isdir(directory_path+os.sep+x)]
+print (directories)
 
 f = open(directory_path + '/RETMeasures.csv', 'w')
 f.write('Case,Algoritmo,Type,CCUU,CCWU,Assortivity,DensityUU,Efficiency,Diameter,CharacteristicPathLength\r\n')
@@ -72,17 +74,56 @@ for directory in directories:
 		#Parse name of file
 
 		nameOutputFile = file.strip('stats').strip('.csv').split('_')
-		caseInfo = ''
-		print (nameOutputFile)
-		if nameOutputFile[4].startswith('RET'): #Check all, and positivo, phototoshop and other shit
-			caseInfo = nameOutputFile [1] + nameOutputFile[2]
+		#print (nameOutputFile)
+		pacient = re.compile('Y[0-2]+[A-B]?')
+		case = re.compile ('[0-9][0-9][B]?[0-9]+[A-B]')
+		algorithmStr = nameOutputFile[0] + nameOutputFile[len(nameOutputFile)-1]
+		pacienteStr = ''
+		caseStr = ''
+		markerStr = ''
+		for position in nameOutputFile:
+			if pacient.match(position):
+				pacienteStr = position
+			elif case.match(position):
+				caseStr = position
+			elif position.startswith('RET'):
+				markerStr = 'RET'
+			elif position.startswith('COL'):
+				markerStr = 'COL'
+			elif position.startswith('GAG'):
+				markerStr = 'GAG'
+			elif position.startswith('CD31'):
+				markerStr = 'CD31'
+			elif position.startswith('CD45'):
+				markerStr = 'CD45'
+			elif position.startswith('CD7'):
+				markerStr = 'CD7'
+			elif position.startswith('CD4'):
+				markerStr = 'CD4'
+			elif position.startswith('CD8'):
+				markerStr = 'CD8'
+			elif position.startswith('CD11b'):
+				markerStr = 'CD11b'
+			elif position.startswith('CD11c'):
+				markerStr = 'CD11c'
+			elif position.startswith('CD20'):
+				markerStr = 'CD20'
+			elif position.startswith('CD68'):
+				markerStr = 'CD68'
+			elif position.startswith('CD163'):
+				markerStr = 'CD163'
+			elif position.startswith('OCT4'):
+				markerStr = 'OCT4'
+			elif position.startswith('S100A6'):
+				markerStr = 'S100A6'
 
-		# outputFile = open(nameOutputFile, 'a')
+		#print (pacienteStr + caseStr + markerStr)
+		outputFile = open(directory_path + os.sep + markerStr + 'Measures.csv' , 'a')
 
-		# f = open(file, 'r')
-		# f.readline()
-		# for line in f:
-		# 	#Case,Algoritmo,Type,CCUU,CCWU,Assortivity,DensityUU,Efficiency,Diameter,CharacteristicPathLength
-		# 	outputFile.write(caseInfo + line)
+		f = open(directory_path + os.sep + directory + os.sep + file, 'r')
+		f.readline()
+		for line in f:
+			#Case,Algoritmo,Type,CCUU,CCWU,Assortivity,DensityUU,Efficiency,Diameter,CharacteristicPathLength
+			outputFile.write(pacienteStr + '_' +caseStr + ',' + algorithmStr + ',' + line)
 
-		# outputFile.close()
+		outputFile.close()
