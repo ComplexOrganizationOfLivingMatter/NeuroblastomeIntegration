778257958358b3e706eeb291511d62693f4cf7d7
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-23 16:23:25 +0200

    radius of that values

--- createNetworkContigousHexagonalGridMeanArea.m
+++ createNetworkContigousHexagonalGridMeanArea.m
@@ -6,12 +6,12 @@ function [ ] = createNetworkContigousHexagonalGridMeanArea()
     lee_imagenes = dir(PathCurrent);
     lee_imagenes = lee_imagenes(3:size(lee_imagenes,1)-1)
     for imK = 1:size(lee_imagenes,1)
-        if (lee_imagenes(imK).isdir == 0) % && (size(strfind(lee_imagenes(imK).name, 'COL'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'CD31'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'RET'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'GAG'),1) == 1)
+        if (lee_imagenes(imK).isdir == 0 && (size(strfind(lee_imagenes(imK).name, 'COL'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'CD31'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'RET'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'GAG'),1) == 1))
             lee_imagenes(imK).name
             Img=imread(lee_imagenes(imK).name);
             Img = Img(:, :, 1);
             Img = im2bw(Img, 0.2);
-            for numMask = 20:20 %[5, 10, 20, 50, 100]
+            for numMask = [5, 10, 15, 25, 50, 100]
                 inNameFile = strsplit(strrep(lee_imagenes(imK).name,' ','_'), '.');
                 outputFileName = strcat('Adjacency\adjacencyMatrix', inNameFile(1), 'ContigousHexagonalMeanAreaMask', num2str(numMask),'Diamet.mat')
 				outputFileNameSif = strcat('visualize\adjacencyMatrix', inNameFile(1), 'ContigousHexagonalMeanAreaMask', num2str(numMask),'Diamet.cvs');
