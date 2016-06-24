f4cd720f22ed1faed3e3ed05f9fa6dea5161cb0d
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-24 13:43:06 +0200

    unnecessary file

--- calculatePersistentHomology.m
+++ calculatePersistentHomology.m
@@ -1,18 +1,34 @@
 %Developed by Pablo Vicente-Munuera
 %Helped by Maria José Jiménez
 
-function [ ] = calculatePersistentHomology()
-    [stat,struc] = fileattrib;
-    PathCurrent = struc.Name;
+function [ ] = calculatePersistentHomology(PathCurrent)
+    load_javaplex
     lee_imagenes = dir(PathCurrent);
     lee_imagenes = lee_imagenes(3:size(lee_imagenes,1)-1)
     for imK = 1:size(lee_imagenes,1)
-        if (lee_imagenes(imK).isdir == 0 && (size(strfind(lee_imagenes(imK).name, 'COL'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'CD31'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'RET'),1) == 1 || size(strfind(lee_imagenes(imK).name, 'GAG'),1) == 1))
+        if (lee_imagenes(imK).isdir == 0 && (size(strfind(lee_imagenes(imK).name, 'COL'),1) == 0 && size(strfind(lee_imagenes(imK).name, 'CD31'),1) == 0 && size(strfind(lee_imagenes(imK).name, 'RET'),1) == 0 && size(strfind(lee_imagenes(imK).name, 'GAG'),1) == 0))
             lee_imagenes(imK).name
             Img=imread(lee_imagenes(imK).name);
             Img = Img(:, :, 1);
             Img = im2bw(Img, 0.2);
+            %pointsImg = generatePointCloudFromImage(Img);
+            C = bwlabel(Img);
+			S = regionprops(C,'Centroid');
+			centroids = vertcat(S.Centroid);
+            %VietorisRips params
+            max_dimension = 2; %We only want holes not anything more
+            max_filtration_value = ;
+            num_divisions = 100;
             
+            stream = api.Plex4.createVietorisRipsStream(centroids, max_dimension, max_filtration_value, num_divisions);
+            persistence = api.Plex4.getModularSimplicialAlgorithm(max_dimension, 2);
+            intervals = persistence.computeIntervals(stream);
+            
+            %Visualization
+            options.filename = 'ripsHouse';
+            options.max_filtration_value = max_filtration_value;
+            options.max_dimension = max_dimension - 1;
+            plot_barcodes(intervals, options);
         end
     end
 end
