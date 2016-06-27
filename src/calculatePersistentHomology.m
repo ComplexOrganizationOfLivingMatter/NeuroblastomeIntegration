6427275372a469553fbc3d1caeac47952ef5060f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-06-27 12:13:16 +0200

    normalize distances and several changes

--- src/calculatePersistentHomology.m
+++ src/calculatePersistentHomology.m
@@ -10,8 +10,8 @@ function [ ] = calculatePersistentHomology(PathCurrent)
             lee_imagenes(imK).name
 			inNameFile = strsplit(strrep(lee_imagenes(imK).name,' ','_'), '.');
 			max_dimension = 2;
-			num_divisions = 20;
-            outputFileName = strcat(PathCurrent, 'Adjacency\persistentHomology', inNameFile(1), 'MaxDim', num2str(max_dimension), '_NumDivision', num2str(20), '.mat');
+			num_divisions = 30;
+            outputFileName = strcat(PathCurrent, 'Adjacency\persistentHomology', inNameFile(1), 'MaxDim', num2str(max_dimension), '_NumDivision', num2str(num_divisions), '.mat');
 			if exist(outputFileName{:}, 'file') ~= 2
 				Img=imread(lee_imagenes(imK).name);
 				Img = Img(:, :, 1);
@@ -20,20 +20,23 @@ function [ ] = calculatePersistentHomology(PathCurrent)
 				C = bwlabel(Img);
 				S = regionprops(C,'Centroid');
 				centroids = vertcat(S.Centroid);
+                centroids = centroids/max(centroids(:));
 				distanceBetweenObjects = pdist(centroids,'euclidean');
 				%VietorisRips params
 				 %We only want holes not anything more
-				max_filtration_value = max(distanceBetweenObjects)/2;
+				max_filtration_value = round(max(distanceBetweenObjects))/2
 				clear Img C S distanceBetweenObjects
 				
-				stream = api.Plex4.createVietorisRipsStream(centroids, max_dimension, max_filtration_value, num_divisions);
-				persistence = api.Plex4.getModularSimplicialAlgorithm(max_dimension, 2);
-				intervals = persistence.computeIntervals(stream);
-				save(outputFileName{:}, 'stream', 'persistence', 'intervals', '-v7.3');
+				stream = api.Plex4.createVietorisRipsStream(centroids, max_dimension, max_filtration_value, num_divisions)
+				persistence = api.Plex4.getModularSimplicialAlgorithm(max_dimension, 2)
+				intervals = persistence.computeIntervals(stream)
+                intervalsStr = char(intervals.toString);
+                intervalsBetti = char(intervals.getBettiNumbers);
+				save(outputFileName{:}, 'intervalsStr', 'intervalsBetti', '-v7.3');
 				clear stream persistence
 				
 				%Visualization
-				outputFileNameImg = strcat(PathCurrent, 'visualize\persistentHomology', inNameFile(1), 'MaxDim', num2str(max_dimension), '_NumDivision', num2str(20), '.mat');
+				outputFileNameImg = strcat(PathCurrent, 'visualize\persistentHomology', inNameFile(1), 'MaxDim', num2str(max_dimension), '_NumDivision', num2str(num_divisions), '.jpg');
 				options.filename = outputFileNameImg{:};
 				options.max_filtration_value = max_filtration_value;
 				options.max_dimension = max_dimension - 1;
