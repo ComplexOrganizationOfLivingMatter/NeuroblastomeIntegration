1a165743e2224ad139b82cbdee7628b1764f4ec4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-07-14 14:04:50 +0200

    the minimumdistance algorithm

--- src/lib/GetConnectedGraphWithMinimumDistancesByIteration.m
+++ src/lib/GetConnectedGraphWithMinimumDistancesByIteration.m
@@ -1,52 +1,53 @@
-function [ adjacencyMatrix ] = GetConnectedGraphWithMinimumDistancesByIteration( distanceBetweenClusters, adjacencyMatrix, C, inNameFile)
+function [ adjacencyMatrixWeights ] = GetConnectedGraphWithMinimumDistancesByIteration( distanceBetweenClusters, adjacencyMatrixWeights, C, inNameFile)
 %GETCONNECTEDGRAPHWITHMINIMUMDISTANCES Summary of this function goes here
 %   Detailed explanation goes here
 %
 %   Developed by Pablo Vicente-Munuera
-
-    if graphconncomp(adjacencyMatrix, 'Directed', 'false') == 1
+    adjacencyMatrix = adjacencyMatrixWeights;
+    if graphconncomp(adjacencyMatrixWeights, 'Directed', 'false') == 1
         outputFileName = strcat('Adjacency\minimumDistanceClasses', inNameFile(1), 'It1.mat')
         outputFileNameSif = strcat('visualize\minimumDistanceClasses', inNameFile(1), 'It1.cvs');
-        save(outputFileName{:}, 'adjacencyMatrix', '-v7.3');
-        generateSIFFromAdjacencyMatrix(adjacencyMatrix, outputFileNameSif{:});
+        save(outputFileName{:}, 'adjacencyMatrixWeights', '-v7.3');
+        generateSIFFromAdjacencyMatrix(adjacencyMatrixWeights, outputFileNameSif{:});
 		return
     end
 
     mDistanceBetweenClusters = squareform(distanceBetweenClusters);
     mDistanceBetweenClusters(logical(eye(size(mDistanceBetweenClusters)))) = intmax('int32');
 
-    i = 1;
     iteration = 1;
     while 1
-        if i > size(adjacencyMatrix,1)
+        if min(sum(adjacencyMatrix(:,:))) >= iteration
             outputFileName = strcat('Adjacency\minimumDistanceClasses', inNameFile(1), 'It', num2str(iteration) ,'.mat')
             outputFileNameSif = strcat('visualize\minimumDistanceClasses', inNameFile(1), 'It', num2str(iteration) ,'.cvs');
             save(outputFileName{:}, 'adjacencyMatrix', '-v7.3');
-            generateSIFFromAdjacencyMatrix(adjacencyMatrix, outputFileNameSif{:});
+            generateSIFFromAdjacencyMatrix(adjacencyMatrixWeights, outputFileNameSif{:});
             
-			if 1 == graphconncomp(adjacencyMatrix, 'Directed', 'false')
+			if 1 == graphconncomp(adjacencyMatrixWeights, 'Directed', 'false')
 				return
-			end
-            i=1;
-            iteration = iteration + 1;
+            end
+            iteration = iteration + 1; 
         end
         minimumDistance = min(mDistanceBetweenClusters(:));
         [rowMin, colMin] = find(mDistanceBetweenClusters == minimumDistance, 1);
         if size(C,2) > 1
            class1 = find(C == rowMin, 1);
             class2 = find(C == colMin, 1);
-            adjacencyMatrix(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
-            adjacencyMatrix(class2, class1) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrixWeights(class1, class2) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrixWeights(class2, class1) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrix(class1, class2) = adjacencyMatrix(class1, class2) + 1;
+            adjacencyMatrix(class2, class1) = adjacencyMatrix(class2, class1) + 1;
+            
         else
-            adjacencyMatrix(rowMin, colMin) = mDistanceBetweenClusters(rowMin, colMin);
-            adjacencyMatrix(colMin, rowMin) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrixWeights(rowMin, colMin) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrixWeights(colMin, rowMin) = mDistanceBetweenClusters(rowMin, colMin);
+            adjacencyMatrix(rowMin, colMin) = adjacencyMatrix(rowMin, colMin) + 1;
+            adjacencyMatrix(colMin, rowMin) = adjacencyMatrix(colMin, rowMin) + 1;
         end
         
         
         mDistanceBetweenClusters(rowMin, colMin) = intmax('int32');
         mDistanceBetweenClusters(colMin, rowMin) = intmax('int32');
-		
-        i = i + 1;
     end
 end
 
