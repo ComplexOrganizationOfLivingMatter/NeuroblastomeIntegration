cd1dc37a1ad0b32015af91aac9fa5bb58d461a7a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-08-02 16:05:17 +0200

    calculating minimum distance to hexagonal grid

--- src/lib/getDistanceMatrixFromHexagonalGrid.m
+++ src/lib/getDistanceMatrixFromHexagonalGrid.m
@@ -1,7 +1,11 @@
-function [ output_args ] = getDistanceMatrixFromHexagonalGrid( input_args )
+function [ distanceMatrix ] = getDistanceMatrixFromHexagonalGrid(Img, mask)
 %GETDISTANCEMATRIXFROMHEXAGONALGRID Summary of this function goes here
 %   Detailed explanation goes here
-
-
+    ImgMasked = Img .* mask;
+    classes = unique(ImgMasked(ImgMasked>0));
+    centroids = regionprops(ImgMasked, 'Centroid');
+    centroidsFiltered = centroids(classes);
+    distanceMatrix = pdist(vertcat(centroidsFiltered.Centroid), 'euclidean');
+    distanceMatrix = squareform(distanceMatrix);
 end
 
