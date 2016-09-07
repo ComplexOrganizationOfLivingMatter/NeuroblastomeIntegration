c2b8867cd4e359dad4ddad3d976b735569241a01
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-07 16:04:28 +0200

    time of algorithm and bugfixes

--- src/minimumDistance.py
+++ src/minimumDistance.py
@@ -2,9 +2,20 @@ import networkx as nx
 import scipy.io
 import h5py
 import numpy as np
+import time
+from time import gmtime, strftime
 
-fileName = '/home/pablo/vboxshare/Neuroblastoma/Datos/Data/Casos/CASO 1. Y01_01B16459B/CoreA/Adjacency/minimumDistanceClasses10_01B16459A_CD11c_negativos_Y01DistanceMatrix.mat'
+start = time.time()
+print strftime("%a, %d %b %Y %H:%M:%S", gmtime())
 
+
+
+fileName = '/home/pablo/vboxshare/Neuroblastoma/Datos/Data/Casos/CASO 1. Y01_01B16459B/CoreA/Adjacency/minimumDistanceClasses12_01B16459A_CD68_negativos_Y01DistanceMatrix.mat'
+#fileName = '/home/pablo/vboxshare/Neuroblastoma/Datos/Data/Casos/CASO 1. Y01_01B16459B/CoreA/Adjacency/minimumDistanceClasses13_01B16459A_CD163_positivos_Y01DistanceMatrix.mat'
+
+
+#print start
+print fileName
 mat = scipy.io.loadmat(fileName)
 distanceMatrix = np.matrix(mat['distanceBetweenObjects'])
 
@@ -24,10 +35,10 @@ while True:
 		outputFileName = fileName.split('/')
 		outputFileName = outputFileName[11].split('.')
 		adjacencyMatrixOut = nx.adjacency_matrix(G)
-		print outputFileName[1].trim('DistanceMatrix')
-		scipy.io.savemat(outputFileName[1].trim('DistanceMatrix') + 'It' + str(iteration) + '.mat', mdict={'adjacencyMatrix': adjacencyMatrixOut.todense()})
+		print outputFileName[0].strip('DistanceMatrix') + 'It' + str(iteration) + '.mat'
+		scipy.io.savemat(outputFileName[0].strip('DistanceMatrix') + 'It' + str(iteration) + '.mat', mdict={'adjacencyMatrix': adjacencyMatrixOut.todense()})
 		iteration = iteration + 1
-		ccomp = nx.connected_components(G)
+		ccomp = sorted(nx.connected_components(G), key = len, reverse=True)
 		
 		if len(ccomp) == 1:
 			break
@@ -44,4 +55,8 @@ while True:
 		edge = (indices[0][index], indices[1][index])
 		G.add_edge(*edge)
 
-	
+
+
+print strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+end = time.time()
+print (end - start)
