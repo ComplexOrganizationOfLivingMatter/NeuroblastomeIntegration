c753a5f7dc86d4f662a719d018480021b1cba147
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-21 15:37:08 +0200

    good way

--- src/getMinimumDistancesFromHexagonalGrid.m
+++ src/getMinimumDistancesFromHexagonalGrid.m
@@ -34,14 +34,14 @@ function [ ] = getMinimumDistancesFromHexagonalGrid( )
                 clear Img
                 if size(distanceMatrix, 1) > 0
                 %--------------------- adjacencyMatrix_minimumDistanceBetweenPairsIt ------------------%
-%                     %Get output file names
-%                     inNameFile = strsplit(strrep(imageName,' ','_'), '.');
-%                     inNameFile = [strcat(inNameFile(1),'_Radius' , num2str(numMask))];
-%                     outputFileName = strcat('E:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\Networks\IterationAlgorithm\minimumDistanceClassesBetweenPairs', inNameFile(1), 'It1.mat')
-%                     if exist(outputFileName{:}, 'file') ~= 2
-%                         %minimumDistance algorithm that outputs an adjacencyMatrix which is connected (i.e. only one connected component).
-%                         GetConnectedGraphWithMinimumDistancesBetweenPairsByIteration(distanceMatrix , sparse(size(distanceMatrix,1), size(distanceMatrix,1)), zeros(1), inNameFile);
-%                     end
+                    %Get output file names
+                    inNameFile = strsplit(strrep(imageName,' ','_'), '.');
+                    inNameFile = [strcat(inNameFile(1),'_Radius' , num2str(numMask))];
+                    outputFileName = strcat('E:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\Networks\IterationAlgorithm\minimumDistanceClassesBetweenPairs', inNameFile(1), 'It1.mat')
+                    if exist(outputFileName{:}, 'file') ~= 2
+                        %minimumDistance algorithm that outputs an adjacencyMatrix which is connected (i.e. only one connected component).
+                        GetConnectedGraphWithMinimumDistancesBetweenPairsByIteration(distanceMatrix , sparse(size(distanceMatrix,1), size(distanceMatrix,1)), zeros(1), inNameFile);
+                    end
                     %--------------------------------------------------------%
 
                     %--------------------- adjacencyMatrix_minimumDistanceIt ------------------%
