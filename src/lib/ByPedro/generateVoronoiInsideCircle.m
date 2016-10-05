00e2fbd5433ca0ee2d14b309bcb344b5b27dc886
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-10-05 16:11:08 +0200

    we get the circle limits and paint it

--- src/lib/ByPedro/generateVoronoiInsideCircle.m
+++ src/lib/ByPedro/generateVoronoiInsideCircle.m
@@ -1,19 +1,8 @@
-function [ ] = generateVoronoiInsideCircle( numIterations, numPoints, radiusOfCircle)
+function [ ] = generateVoronoiInsideCircle( numIterations, numPoints, radiusOfCircle, circleLimits)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 %
 %   Developed by Pablo Vicente-Munuera and Pedro Gomez-Galvez
-    
-    %this is to get the ROI of the original image
-%     h = imellipse(gca, [limite limite diametro diametro]);
-%     api = iptgetapi(h);
-%     
-%     fcn = getPositionConstraintFcn(h);
-%     
-%     api.setPositionConstraintFcn(fcn);
-%     pause
-%     
-%     BW = createMask(h);
 
     initCentroids = Chose_seeds_positions(1, radiusOfCircle*2, radiusOfCircle*2, numPoints, 2);
 
@@ -28,28 +17,23 @@ function [ ] = generateVoronoiInsideCircle( numIterations, numPoints, radiusOfCi
         DL = watershed(D);         %% Separa regiones (celulas) por proximidad
         bgm = DL == 0;             %% Le da valor 1 a los límites y cero al interior de las células
         %Add ones to the difference between the circle and the square
-        circleLimits = strel('disk', radiusOfCircle);
-        circleLimits = [circleLimits.getnhood(), zeros(radiusOfCircle*2 - 1, 1)];
-        circleLimits = [circleLimits; zeros(1, radiusOfCircle*2)];
-        
+        perim = bwperim(circleLimits);
         Voronoi = bgm & circleLimits;
 
-        L_original=bwlabel(1-Voronoi,8);  %% De esta manera da una etiqueta a todas las celulas y da valor 1 a todas las celulas del borde
-
+        L_original = bwlabel(1 - Voronoi, 8);  %% De esta manera da una etiqueta a todas las celulas y da valor 1 a todas las celulas del borde
+        L_original = L_original & (1 - perim);
+        L_original = L_original & (circleLimits);
         %%Obtenemos nuevos centroides
         centro = regionprops(L_original,'Centroid');
         centros_nuevos = cat(1, centro.Centroid);
         centros_nuevos=round(centros_nuevos);
-        centros_nuevos=fliplr(centros_nuevos); %[filas,columnas]
+        %centros_nuevos=fliplr(centros_nuevos); %[filas,columnas]
         initCentroids=centros_nuevos;
-
-
-
-        FOLDER='E:\Pablo\Neuroblastoma\NeuroblastomeIntegration\TempResults';
-        Archivo=strcat('Imagen_',num2str(i),'_Diagrama_',num2str(j),'_Vonoroi_1');
-
-        save(strcat(FOLDER,'\',Archivo),'L_original')
-
-        imwrite(L_original, (strcat(FOLDER,'\',Archivo,'.png')))
     end
+    FOLDER='E:\Pablo\Neuroblastoma\NeuroblastomeIntegration\TempResults';
+    Archivo=strcat('Imagen_',num2str(i),'_Diagrama_',num2str(j),'_Vonoroi_1');
+    
+    save(strcat(FOLDER,'\',Archivo),'L_original')
+    
+    imwrite(L_original, (strcat(FOLDER,'\',Archivo,'.png')))
 end
