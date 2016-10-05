68f469fc03061fedc82c217e2898045d1e695d8a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-10-05 14:24:56 +0200

    inside circle but not working

--- src/lib/ByPedro/generateVoronoiInsideCircle.m
+++ src/lib/ByPedro/generateVoronoiInsideCircle.m
@@ -29,15 +29,14 @@ function [ ] = generateVoronoiInsideCircle( numIterations, numPoints, radiusOfCi
         bgm = DL == 0;             %% Le da valor 1 a los límites y cero al interior de las células
         %Add ones to the difference between the circle and the square
         circleLimits = strel('disk', radiusOfCircle);
-        circleLimits = [circleLimits.getnhood(); zeros(radiusOfCircle*2 - 1, 1)];
+        circleLimits = [circleLimits.getnhood(), zeros(radiusOfCircle*2 - 1, 1)];
         circleLimits = [circleLimits; zeros(1, radiusOfCircle*2)];
         
-        Voronoi = bgm;
+        Voronoi = bgm & circleLimits;
 
         L_original=bwlabel(1-Voronoi,8);  %% De esta manera da una etiqueta a todas las celulas y da valor 1 a todas las celulas del borde
 
         %%Obtenemos nuevos centroides
-
         centro = regionprops(L_original,'Centroid');
         centros_nuevos = cat(1, centro.Centroid);
         centros_nuevos=round(centros_nuevos);
