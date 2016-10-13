cc72e18c726f40eed68cf2b7a13cc95779d7c57d
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-10-13 14:23:42 +0200

    we have to unify those cell array

--- src/analyzeGraphletsDistances.m
+++ src/analyzeGraphletsDistances.m
@@ -3,16 +3,27 @@ function [ ] = analyzeGraphletsDistances( )
 %   Detailed explanation goes here
     graphletFiles = getAllFiles('E:\Pablo\Neuroblastoma\Results\graphletsCount\NuevosCasos\RET\NDUMP2\DivideByPacient\');
 
+    
+        pacientArray = {};
+        sortingWTMeanArray = {};
+        sortingWTSizeArray = {};
+        differenceGraphletsSortingArray = {};
+        iterationWTMeanArray = {};
+        iterationWTSizeArray = {};
+        differenceGraphletsIterationArray = {};
+    
     for numFile = 1:size(graphletFiles,1)
+        
         fullPathGraphlet = graphletFiles{numFile};
         graphletNameSplitted = strsplit(fullPathGraphlet, '\');
+        graphletNameSplitted(end-1)
         graphletName = graphletNameSplitted(end);
         graphletName = graphletName{1};
-        
-        fid = fopen(strcat(strjoin(graphletNameSplitted(1:end-2), '\') ,'caracteristics.csv'), 'w');
+
+
         
         if size(strfind(graphletName, 'gcd73'), 1) > 0
-            distanceMatrix = importfile(fullPathGraphlet);
+            distanceMatrix = dlmread(fullPathGraphlet, '\t', 1, 1);
             names = importfileNames(fullPathGraphlet);
             algorithmsFilter = cellfun(@(x) size(strfind(x, 'BetweenPairs'), 1) > 0, names);
 
@@ -21,23 +32,44 @@ function [ ] = analyzeGraphletsDistances( )
             sortingControlFilter = cellfun(@(x) size(strfind(x, 'Control'), 1) > 0, sortingNames);
             sortingWTNames = sortingNames(sortingControlFilter == 0);
             sortingWT = sortingAlgorithm(sortingControlFilter == 0, sortingControlFilter);
-            sortingWT = mean(sortingWT, 2);
+            sortingWTMean = mean(sortingWT, 2)';
+            differenceGraphletsSorting = zeros(size(sortingWTMean, 2) - 1, 1);
+            
+            for i = 1:size(differenceGraphletsSorting, 1)
+                differenceGraphletsSorting(i) = sortingWTMean(i + 1) - sortingWTMean(i);
+            end
 
             iterationAlgorithm = distanceMatrix(algorithmsFilter == 1, algorithmsFilter == 1);
             iterationNames = names(algorithmsFilter);
             iterationControlFilter = cellfun(@(x) size(strfind(x, 'Control'), 1) > 0, iterationNames);
-            iterationWTNames = sortingNames(iterationControlFilter == 0);
+            iterationWTNames = iterationNames(iterationControlFilter == 0);
             iterationWT = iterationAlgorithm(iterationControlFilter == 0, iterationControlFilter);
-            iterationWT = mean(iterationWT, 2);
+            iterationWTMean = mean(iterationWT, 2)';
+            differenceGraphletsIteration = zeros(size(iterationWTMean, 2) - 1, 1);
+            
+            for i = 1:size(differenceGraphletsIteration, 1)
+                differenceGraphletsIteration(i) = iterationWTMean(i + 1) - iterationWTMean(i);
+            end
             
             outputFile = strjoin(graphletNameSplitted(1:end-1), '\');
-            save(strcat(outputFile, '\meanDistanceWithControl.mat'), 'sortingWTNames', 'sortingWT', 'iterationWTNames', 'iterationWT');
+            save(strcat(outputFile, '\meanDistanceWithControl.mat'), 'sortingWTNames', 'sortingWTMean', 'iterationWTNames', 'iterationWTMean');
             
-            fprintf(fid, 
+            pacientArray(end+1) = {graphletNameSplitted(end-1)};
+            sortingWTMeanArray(end+1) = {sortingWTMean};
+            sortingWTSizeArray(end+1) = {size(sortingWTMean, 2)};
+            differenceGraphletsSortingArray(end+1) = {differenceGraphletsSorting};
+            iterationWTMeanArray(end+1) = {iterationWTMean};
+            iterationWTSizeArray(end+1) = {size(iterationWTMean, 2)};
+            differenceGraphletsIterationArray(end+1) = {differenceGraphletsIteration};
+
         end
-        
-        fclose(fid);
     end
+    
+    
+    padcat(sortingWTMeanArray{:}, differenceGraphletsSortingArray{:});
+    fid = fopen(strcat(strjoin(graphletNameSplitted(1:end-2), '\') ,'caracteristics.csv'), 'w');
+
+    fclose(fid);
 
 end
 
