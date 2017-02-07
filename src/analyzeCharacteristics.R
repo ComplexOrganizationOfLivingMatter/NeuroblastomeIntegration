268dfc771930e71af4dec865332ee0c931569629
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-02-07 11:29:09 +0100

    som

--- src/analyzeCharacteristics.R
+++ src/analyzeCharacteristics.R
@@ -4,11 +4,27 @@
 
 source('E:/Pablo/Neuroblastoma/NeuroblastomeIntegration/src/analyzeCharacteristicsGDDA.R')
 source('E:/Pablo/Neuroblastoma/NeuroblastomeIntegration/src/saveQPlot.R')
+library("kohonen", lib.loc="~/R/win-library/3.3")
 
 startingColumn <- 3
-characteristics <- analyzeCharacteristicsGDDA("../Results/graphletsCount/NuevosCasos/Analysis/Characteristics_GDDA_AgainstControl_Inestability_0sInsteadOf-1s_30_01_2017.csv", startingColumn);
-#characteristics <- analyzeCharacteristicsGDDA("../Results/graphletsCount/NuevosCasos/Analsys/Characteristics_GDDA_AgainstControl_UHR_30_01_2017.csv", startingColumn);
-#characteristics <- characteristics[characteristics[, startingColumn - 1] == "Media" | characteristics[, startingColumn - 1] == "Alta", ]
+#characteristics <- analyzeCharacteristicsGDDA("../Results/graphletsCount/NuevosCasos/Analysis/Characteristics_GDDA_AgainstControl_Inestability_0sInsteadOf-1s_30_01_2017.csv", startingColumn);
+characteristics <- analyzeCharacteristicsGDDA("../Results/graphletsCount/NuevosCasos/Analysis/Characteristics_GDDA_AgainstControl_Risk_0sInsteadOf-1s_03_02_2017.csv", startingColumn);
+
+characteristics <- analyzeCharacteristicsGDDA("../Results/graphletsCount/NuevosCasos/Analysis/testOverfitting_07_02_2017.csv", startingColumn);
+#characteristics <- characteristics[characteristics[, startingColumn - 1] == "Baja" | characteristics[, startingColumn - 1] == "Media", ]
+
+classesCharacteristics <- classvec2classmat(t(characteristics[, startingColumn - 1]))
+#som2 <- bdk(as.matrix(characteristics[, c(3, 5, 7)]), grid = somgrid(xdim = 2, ydim = 2, topo = c("rectangular", "hexagonal")), classesCharacteristics);
+som2 <- bdk(as.matrix(characteristics[, startingColumn:length(characteristics)]), grid = somgrid(xdim = 1, ydim = 2, topo = c("rectangular", "hexagonal")), classesCharacteristics);
+plot(som2)
+
+
+som.wines <- som(Xtraining, grid = somgrid(5, 5, "hexagonal"))
+som.prediction <- predict(som.wines, newdata = Xtest,
+                          trainX = Xtraining,
+                          trainY = factor(wine.classes[training]))
+table(wine.classes[-training], som.prediction$prediction)
+
 
 tsneCoordinates <- tsne(characteristics[, startingColumn:length(characteristics)], perplexity = 30)
 gplot <- qplot(tsneCoordinates[,1], tsneCoordinates[,2], colour=characteristics[, 2])
