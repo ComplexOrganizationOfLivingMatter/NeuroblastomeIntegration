65557d9141e81543d230c0cba0a8028bb40e2f94
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-02-24 14:34:51 +0100

    no pdf

--- src/saveQPlot.R
+++ src/saveQPlot.R
@@ -2,7 +2,7 @@ saveQPlot <- function(infoFile, gplot, height = 1024, width = 1024, directory = 
   
   filename2 <- paste(directory, 'qplot', infoFile, sep = "")
   
-  ggsave(paste(filename2, '.pdf', sep=""), gplot, width = width/100, height = height/100)
+  #ggsave(paste(filename2, '.pdf', sep=""), gplot, width = width/100, height = height/100)
   
   png(paste(filename2, '.png', sep=""), width = width, height = height, res = 150)
   
