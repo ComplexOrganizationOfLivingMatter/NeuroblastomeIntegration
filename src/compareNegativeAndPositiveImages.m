166f4ec5871db342dac1f879a7ee927640a415c6
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-01 12:29:06 +0100

    reading images

--- src/compareNegativeAndPositiveImages.m
+++ src/compareNegativeAndPositiveImages.m
@@ -2,8 +2,9 @@ function [ ] = compareNegativeAndPositiveImages( currentPath, markerWeWant )
 %COMPARENEGATIVEANDPOSITIVEIMAGES Summary of this function goes here
 %   Detailed explanation goes here
     allFiles = getAllFiles(currentPath);
-    for imK = 1:size(allFiles,1)
-        fullPathFileName = allFiles(imK);
+    allFiles = sort(allFiles);
+    for numFile = 1:size(allFiles,1)
+        fullPathFileName = allFiles(numFile);
         fullPathFileName = fullPathFileName{:};
         fullPathFileNameSplitted = strsplit(fullPathFileName, '\');
         fileName = fullPathFileNameSplitted(end);
@@ -11,7 +12,15 @@ function [ ] = compareNegativeAndPositiveImages( currentPath, markerWeWant )
         %Path name
         basePath = strjoin(fullPathFileNameSplitted(1:6), '\');
         %If it's a directory and positive image (marker positive). Also we exclude the files of col, ret, CD31 and GAG
-        if isempty(strfind(lower(fullPathFileName), lower(markerWeWant))) == 0
+        if isempty(strfind(lower(fullPathFileName), lower(markerWeWant))) == 0 && isempty(strfind(lower(fullPathFileName), lower('CELS'))) == 1
+            imgNegative = imread(allFiles{numFile-1});
+            imgNegative = im2bw(imgNegative(:,:,1), 0.2);
+            imgNegativeLog = logical(imgNegative);
+            
+            imgPositive = imread(allFiles{numFile-1});
+            imgPositive = im2bw(imgPositive(:,:,1), 0.2);
+			imgPositiveLog = logical(imgPositive);
+            
             
         end
     end
