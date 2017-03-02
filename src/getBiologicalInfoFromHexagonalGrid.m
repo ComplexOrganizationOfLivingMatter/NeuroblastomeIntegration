f00d9f817dc72cb28dde550b1ec2d47a99517b8a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-02 11:10:25 +0100

    quantity of branches per cell

--- src/getBiologicalInfoFromHexagonalGrid.m
+++ src/getBiologicalInfoFromHexagonalGrid.m
@@ -1,24 +1,37 @@
-function [percentageOfFibrePerFilledCell, quantityOfFibrePerFilledCell, percentageOfFibrePerCell, quantityOfFibrePerCell, distanceMatrix, centroidsFiltered, ImgMasked] = getBiologicalInfoFromHexagonalGrid(image, mask)
+function [quantityOfBranchesFilledPerCell, quantityOfBranchesPerCell, percentageOfFibrePerFilledCell, quantityOfFibrePerFilledCell, percentageOfFibrePerCell, quantityOfFibrePerCell, distanceMatrix, centroidsFiltered, ImgMasked] = getBiologicalInfoFromHexagonalGrid(image, mask)
 %GETBIOLOGICALINFOFROMHEXAGONALGRID Summary of this function goes here
-%   Detailed explanation goes here
+    %   Detailed explanation goes here
     [ distanceMatrix, centroidsFiltered, ImgMasked, classes] = getDistanceMatrixFromHexagonalGrid(image, mask);
+
+    imgSkel = bwmorph(image,'skel',inf);
+    imgBranchingPoints = bwmorph(imgSkel,'branchpoints');
+
+    imgBranchingPoints = imgBranchingPoints .* ImgMasked;
+%     figure;
+%     subplot(1,2,1), subimage(imgPositiveLogSkel)
+%     subplot(1,2,2), subimage(imgPositiveBranchingPoints)
     
     lengthAllCells = max(max(mask));
     quantityOfFibrePerFilledCell = zeros(length(centroidsFiltered), 1);
     percentageOfFibrePerFilledCell = zeros(length(centroidsFiltered), 1);
+    quantityOfBranchesPerFilledCell = zeros(length(centroidsFiltered), 1);
     quantityOfFibrePerCell = zeros(lengthAllCells, 1);
     percentageOfFibrePerCell = zeros(lengthAllCells, 1);
+    quantityOfBranchesPerCell = zeros(lengthAllCells, 1);
     parfor i = 1:lengthAllCells
         num = classes(classes == i);
         if num > 0
+            quantityOfBranchesPerCell(i) = sum(sum(imgBranchingPoints == num));
             quantityOfFibrePerCell(i) = sum(sum(ImgMasked == num));
-            percentageOfFibrePerCell(i) = sum(sum(mask == num));
+            percentageOfFibrePerCell(i) = sum(sum(mask == num)); %total pixels per cell
         else
+            quantityOfBranchesPerCell(i) = 0;
             quantityOfFibrePerCell(i) = 0;
             percentageOfFibrePerCell(i) = 1;
         end
     end
     
+    quantityOfBranchesFilledPerCell = quantityOfBranchesPerCell(classes);
     quantityOfFibrePerFilledCell = quantityOfFibrePerCell(classes);
     percentageOfFibrePerFilledCell = percentageOfFibrePerCell(classes);
     
