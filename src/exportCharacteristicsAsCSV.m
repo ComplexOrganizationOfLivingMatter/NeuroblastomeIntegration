4385277974a3269a0a60e13666184400932ffa26
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-02 15:08:46 +0100

    new characteristics

--- src/exportCharacteristicsAsCSV.m
+++ src/exportCharacteristicsAsCSV.m
@@ -3,14 +3,20 @@ function [ ] = exportCharacteristicsAsCSV( pathOfInfo )
 %   Detailed explanation goes here
     
     allImages = getAllFiles(pathOfInfo);
-    characteristics = cell(length(allImages), 5);
+    characteristics = cell(length(allImages), 7);
     for numImg = 1:size(allImages,1)
         fullPathFile = allImages{numImg};
         fileNameSplitted = strsplit(fullPathFile, '\');
         fileName = fileNameSplitted(end);
         fileName = fileName{1};
+        fileNameSplittedSplitted = strsplit(fileName, '_');
+        if isempty(fileNameSplittedSplitted{1})
+            fileNameFinal = fileNameSplittedSplitted{2};
+        else
+            fileNameFinal = fileNameSplittedSplitted{1};
+        end
         load(fullPathFile);
-        characteristics(numImg, :) = {fileName(1:end-61), meanPercentageOfFibrePerCell, stdPercentageOfFibrePerCell, meanPercentageOfFibrePerFilledCell, stdPercentageOfFibrePerFilledCell};
+        characteristics(numImg, :) = {fileNameFinal, meanPercentageOfFibrePerCell, stdPercentageOfFibrePerCell, meanPercentageOfFibrePerFilledCell, stdPercentageOfFibrePerFilledCell, meanQuantityOfBranchesPerCell, meanQuantityOfBranchesFilledPerCell};
     end
     characteristicsTable = cell2table(characteristics);
     writetable(characteristicsTable, strcat(strjoin(fileNameSplitted(1:end - 2), '\'), '\characteristics', fileNameSplitted{end - 3} ,'.xls'));
