2092e77500d90061448b119df70fa47a56238d3c
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-04-07 13:28:53 +0200

    controls for all except negative cells images

--- src/getMinimumDistancesFromHexagonalGrid.m
+++ src/getMinimumDistancesFromHexagonalGrid.m
@@ -57,7 +57,7 @@ function [ ] = getMinimumDistancesFromHexagonalGrid(PathCurrent, markerName)
                         distanceMatrix = [];
                     end
                 end
-                if size(distanceMatrix, 1) > 15
+                if size(distanceMatrix, 1) > 15 && if isempty(strfind(lower(imageName), lower('NEG')), 1) == 1
                     createNetworksWithControls(fullPathImage, Img, distanceMatrix, basePath, numMask, inNameFile, imageName);
                 end
             end
