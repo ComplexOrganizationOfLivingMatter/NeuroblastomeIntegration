72ea77851db641c6aacbb5779404aa95b260a8ba
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-24 15:43:37 +0200

    testing how should be the control without broken part

--- src/lib/ByPedro/generateVoronoiInsideCircle.m
+++ src/lib/ByPedro/generateVoronoiInsideCircle.m
@@ -1,29 +1,34 @@
-function [ ] = generateVoronoiInsideCircle( numIterations, numPoints, radiusOfCircle, circleLimits, fileName)
+function [ ] = generateVoronoiInsideCircle( numIterations, numPoints, radiusOfCircle, mask, fileName)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 %
 %   Developed by Pablo Vicente-Munuera and Pedro Gomez-Galvez
 
-    initCentroids = Chose_seeds_positions(1, radiusOfCircle(1)*2, radiusOfCircle(2)*2, numPoints, 5);
+    initCentroids = Chose_seeds_positions(mask, numPoints, 5);
     size(initCentroids, 1)
     j = 1;
     while j <= numIterations
-        image = zeros(radiusOfCircle(1)*2, radiusOfCircle(2)*2); % Se define imagen que contiene puntos
+        image = zeros(size(mask)); % Se define imagen que contiene puntos
         j
         for k = 1:size(initCentroids,1)
             image(initCentroids(k,1), initCentroids(k,2)) = 1;
         end
 
-        D = bwdist(image);        %% Calcula las distancias más próximas de las casillas a la semilla
-        DL = watershed(D);         %% Separa regiones (celulas) por proximidad
-        bgm = DL == 0;             %% Le da valor 1 a los límites y cero al interior de las células
+        D = bwdist(image);          %% Calcula las distancias más próximas de las casillas a la semilla
+        D(mask == 0) = 0;
+        DL = watershed(D);          %% Separa regiones (celulas) por proximidad
+        %DL(mask == 0) = 0;
+        bgm = DL == 0;              %% Le da valor 1 a los límites y cero al interior de las células
         %Add ones to the difference between the circle and the square
-        perim = bwperim(circleLimits);
-        Voronoi = bgm & circleLimits;
+        perim = bwperim(mask);
+        Voronoi = bgm & mask;
+        Voronoi(perim == 1) = 1;
+        
+        
 
         L_original = bwlabel(1 - Voronoi, 8);  %% De esta manera da una etiqueta a todas las celulas y da valor 1 a todas las celulas del borde
         L_original = L_original & (1 - perim);
-        L_original = L_original & (circleLimits);
+        L_original = L_original & (mask);
         %%Obtenemos nuevos centroides
         centro = regionprops(L_original);
         centros_nuevos = cat(1, centro.Centroid);
@@ -40,7 +45,7 @@ function [ ] = generateVoronoiInsideCircle( numIterations, numPoints, radiusOfCi
         
         if size(initCentroids, 1) ~= numPoints
             j = 0;
-            initCentroids = Chose_seeds_positions(1, radiusOfCircle(1)*2, radiusOfCircle(2)*2, numPoints, 5);
+            initCentroids = Chose_seeds_positions(mask, numPoints, 5);
             'init centroid again'
         end
         j = j + 1;
