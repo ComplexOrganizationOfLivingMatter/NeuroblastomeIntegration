d51d16c4d1072eb44b30115b71527a4708dba7ac
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-24 10:21:30 +0200

    title correct and other info added

--- src/paintNetworkIntoARealImage.m
+++ src/paintNetworkIntoARealImage.m
@@ -1,19 +1,24 @@
-function [ ] = paintNetworkIntoARealImage( imageName, adjacencyMatrix, centroids )
+function [ ] = paintNetworkIntoARealImage( imageName, adjacencyMatrix, centroids, pacientName, algorithmUsed )
 %PAINTNETWORKINTOAREALIMAGE Summary of this function goes here
 %   Detailed explanation goes here
-    h = figure;
-    centroids = {centroids.Centroid};
-    centroids = padcat(centroids{:});
+    h = figure('visible', 'off');
+    %centroids = {centroids.Centroid};
+    %centroids = padcat(centroids{:});
     img = imread(imageName);
-    image(img);
+    %Only network
+    %img(img>0) = 0;
+    imshow(img);
     colormap hot;
     hold on;
     for i = 1:size(adjacencyMatrix, 1)
         for j = 1:size(adjacencyMatrix, 2)
             if adjacencyMatrix(i, j) > 0
                 plot([centroids(i, 1), centroids(j, 1)], [centroids(i, 2), centroids(j, 2)], 'r-');
+                %plot([centroids(i, 1), centroids(j, 1)], [centroids(i, 2), centroids(j, 2)], 'r.');
             end
         end
     end
+    title ([pacientName, ' ', algorithmUsed]);
+    export_fig(h, strcat(pacientName, '_', algorithmUsed, '.pdf'), '-r300', '-painters')
 end
 
