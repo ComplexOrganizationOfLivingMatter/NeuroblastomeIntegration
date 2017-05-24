eaa519530ddb61be7b8c337842eefdda9fc5c2c6
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-24 13:49:52 +0200

    only want a part of the mask

--- src/lib/removingArtificatsFromImage.m
+++ src/lib/removingArtificatsFromImage.m
@@ -3,9 +3,32 @@ function [ newMask ] = removingArtificatsFromImage( mask, originalImage )
 %   Detailed explanation goes here
     originalImgGray = rgb2gray(originalImage);
     imgBin = originalImgGray < 255;
-    segmentedAreas = regionprops(imgBin, 'Area', 'PixelList');
-    areasOfHoles = vertcat(segmentedAreas.Area);
-    pixelsToRemoveFromMask = segmentedAreas(areasOfHoles > 100).PixelList;
+    maskOfBiopsy = 1 - bwareaopen(1-imgBin, 1000000);
+    boundingBox = regionprops(maskOfBiopsy, 'BoundingBox');
+    
+    figure;
+    %roiImage = imread(fullPathImage);
+    imshow(imgBin);
+    h = imellipse(gca, boundingBox.BoundingBox);
+    api = iptgetapi(h);
+
+    fcn = getPositionConstraintFcn(h);
+
+    api.setPositionConstraintFcn(fcn);
+
+    maskImage2 = createMask(h);
+    close all
+    
+    imgBin(maskImage2 == 0) = 1;
+    
+    imgBinDilated = imdilate(imgBin, strel('disk', 20));
+    %The holes we want to eliminate but dilated
+    pixelsOfGoodAreas = regionprops(imgBinDilated, 'PixelList');
+    %All the holes
+    segmentedAreasReal = regionprops(imgBin, 'PixelList');
+    allHolePixels = {segmentedAreasReal.PixelList};
+    
+    pixelsToRemoveFromMask = segmentedAreasReal(finalRemovableAreas).PixelList;
     pixelsToRemoveFromMask = fliplr(pixelsToRemoveFromMask);
     imshow(mask);
     newMask = mask;
