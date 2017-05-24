8e6583aafd5739bc3053abd7e7180c920330c9fa
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-24 13:12:53 +0200

    removing from the mask

--- src/lib/removingArtificatsFromImage.m
+++ src/lib/removingArtificatsFromImage.m
@@ -3,6 +3,17 @@ function [ newMask ] = removingArtificatsFromImage( mask, originalImage )
 %   Detailed explanation goes here
     originalImgGray = rgb2gray(originalImage);
     imgBin = originalImgGray < 255;
-    segmented = regionprops(1 - imgBin);
+    segmentedAreas = regionprops(imgBin, 'Area', 'PixelList');
+    areasOfHoles = vertcat(segmentedAreas.Area);
+    pixelsToRemoveFromMask = segmentedAreas(areasOfHoles > 100).PixelList;
+    pixelsToRemoveFromMask = fliplr(pixelsToRemoveFromMask);
+    imshow(mask);
+    newMask = mask;
+    %Remove the pixels of the big holes
+    for numRow = 1:size(pixelsToRemoveFromMask, 1)
+        newMask(pixelsToRemoveFromMask(numRow, 1), pixelsToRemoveFromMask(numRow, 2)) = 0;
+    end
+    figure;
+    imshow(newMask);
 end
 
