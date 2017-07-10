e0f447746054229bc1376e426ebdbc82deac055f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-10 12:42:51 +0200

    adding filter and euler number

--- src/exportCharacteristicsAsCSV.m
+++ src/exportCharacteristicsAsCSV.m
@@ -1,9 +1,12 @@
-function [ ] = exportCharacteristicsAsCSV( pathOfInfo )
+function [ ] = exportCharacteristicsAsCSV( pathOfInfo, filter )
 %EXPORTCHARACTERISTICSASCSV Summary of this function goes here
 %   Detailed explanation goes here
     
     allImages = getAllFiles(pathOfInfo);
-    characteristics = cell(length(allImages), 7);
+    if isempty(filter) == 0
+        allImages = allImages(cellfun(@(x) isempty(strfind(lower(x), lower(filter))) == 0, allImages));
+    end
+    characteristics = cell(length(allImages), 8);
     for numImg = 1:size(allImages,1)
         fullPathFile = allImages{numImg};
         fileNameSplitted = strsplit(fullPathFile, '\');
@@ -16,9 +19,13 @@ function [ ] = exportCharacteristicsAsCSV( pathOfInfo )
             fileNameFinal = fileNameSplittedSplitted{1};
         end
         load(fullPathFile);
-        characteristics(numImg, :) = {fileNameFinal, meanPercentageOfFibrePerCell, stdPercentageOfFibrePerCell, meanPercentageOfFibrePerFilledCell, stdPercentageOfFibrePerFilledCell, meanQuantityOfBranchesPerCell, meanQuantityOfBranchesFilledPerCell};
+        characteristics(numImg, :) = {fileNameFinal, meanPercentageOfFibrePerCell, stdPercentageOfFibrePerCell, meanPercentageOfFibrePerFilledCell, stdPercentageOfFibrePerFilledCell, meanQuantityOfBranchesPerCell, meanQuantityOfBranchesFilledPerCell, eulerNumber};
     end
     characteristicsTable = cell2table(characteristics);
-    writetable(characteristicsTable, strcat(strjoin(fileNameSplitted(1:end - 2), '\'), '\characteristics', fileNameSplitted{end - 3} ,'.xls'));
+    if isempty(filter)
+        writetable(characteristicsTable, strcat(strjoin(fileNameSplitted(1:end - 2), '\'), '\characteristics', fileNameSplitted{end - 3} ,'.xls'));
+    else
+        writetable(characteristicsTable, strcat(strjoin(fileNameSplitted(1:end - 2), '\'), '\characteristics', fileNameSplitted{end - 3} , '_', filter, '.xls'));
+    end
 end
 
