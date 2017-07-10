fa5ee2bc6ffbcc7de443f48bcf0829f5fc287ac2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-10 16:34:39 +0200

    grouping by patient

--- src/analysis3D.m
+++ src/analysis3D.m
@@ -6,11 +6,28 @@ function [ ] = analysis3D( imagesPath, possibleMarkers )
     onlyImagesFiles = allFiles(onlyImagesFiles);
     
     onlyImagesFilesNoMasks = cellfun(@(x) isempty(strfind(lower(x), 'mask')) & isempty(strfind(lower(x), 'neg')) & isempty(strfind(lower(x), 'cel')), onlyImagesFiles);
-    
     onlyImagesFilesNoMasks = onlyImagesFiles(onlyImagesFilesNoMasks);
+    
+    patientOfImagesSplitted = cellfun(@(x) strsplit(x, '\'), onlyImagesFilesNoMasks, 'UniformOutput', false);
+    patientOfImages = cellfun(@(x) strsplit(x{end-1}, '_'), patientOfImagesSplitted, 'UniformOutput', false);
+    
+    patientOfImagesOnlyCase = cell(size(patientOfImages, 1), 1);
+    for numImage = 1:size(patientOfImages, 1)
+        newCase = patientOfImages{numImage};
+        if size(newCase, 2) > 1
+            if newCase{2}(1) == 'A' || newCase{2}(1) == 'B' || isempty(newCase{1})
+                patientOfImagesOnlyCase(numImage) = {strjoin(newCase(1:2), '')};
+            else
+                patientOfImagesOnlyCase(numImage) = newCase(1);
+            end
+        else
+            patientOfImagesOnlyCase(numImage) = {newCase};
+        end
+    end
+    
     %Filtering by markers
-    filterOfMarkers = zeros(size(onlyImagesFilesNoMasks, 1), size(possibleMarkers, 1));
-    for numMarker = 1:size(possibleMarkers, 1)
+    filterOfMarkers = zeros(size(onlyImagesFilesNoMasks, 1), size(possibleMarkers, 2));
+    for numMarker = 1:size(possibleMarkers, 2)
         filterOfMarkers(:, numMarker) = cellfun(@(x) isempty(strfind(lower(x), lower(possibleMarkers{numMarker}))) == 0, onlyImagesFilesNoMasks);
     end
     
