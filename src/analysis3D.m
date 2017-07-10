7b56892e49b2c0dc41ab1d38b604b726f5b5ef45
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-10 16:10:46 +0200

    removing masks ant .txt

--- src/analysis3D.m
+++ src/analysis3D.m
@@ -2,16 +2,18 @@ function [ ] = analysis3D( imagesPath, possibleMarkers )
 %ANALYSIS3D Summary of this function goes here
 %   Detailed explanation goes here
     allFiles = getAllFiles(imagesPath);
-    onlyImagesFiles = cellfun(@(x) isempty(strfind(lower(x), lower('\Images\'))) == 0, allFiles);
+    onlyImagesFiles = cellfun(@(x) isempty(strfind(lower(x), lower('\Images\'))) == 0 & isempty(strfind(lower(x), lower('.txt'))), allFiles);
     onlyImagesFiles = allFiles(onlyImagesFiles);
     
-    onlyImageFilesNoMasks = cellfun(@(x) isempty(strfind(lower(x), 'mask')) | isempty(strfind(lower(x), 'neg')), onlyImageFiles);
+    onlyImagesFilesNoMasks = cellfun(@(x) isempty(strfind(lower(x), 'mask')) & isempty(strfind(lower(x), 'neg')) & isempty(strfind(lower(x), 'cel')), onlyImagesFiles);
     
-    
+    onlyImagesFilesNoMasks = onlyImagesFiles(onlyImagesFilesNoMasks);
     %Filtering by markers
-    filterOfMarkers = zeros(size(onlyImagesFiles, 1), size(possibleMarkers, 1));
+    filterOfMarkers = zeros(size(onlyImagesFilesNoMasks, 1), size(possibleMarkers, 1));
     for numMarker = 1:size(possibleMarkers, 1)
-        filterOfMarkers(:, numMarker) = cellfun(@(x) isempty(strfind(lower(x), lower(possibleMarkers{numMarker}))) == 0, onlyImagesFiles);
+        filterOfMarkers(:, numMarker) = cellfun(@(x) isempty(strfind(lower(x), lower(possibleMarkers{numMarker}))) == 0, onlyImagesFilesNoMasks);
     end
+    
+    filterOfMarkers
 end
 
