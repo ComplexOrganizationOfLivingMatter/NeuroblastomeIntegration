d0b0c86f0272e83009c59f0c0ccbc4b5c20dd3c3
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-10 16:00:44 +0200

    filtering all the files and dividing by markers

--- src/analysis3D.m
+++ src/analysis3D.m
@@ -1,7 +1,17 @@
-function [ ] = analysis3D( imagesPath )
+function [ ] = analysis3D( imagesPath, possibleMarkers )
 %ANALYSIS3D Summary of this function goes here
 %   Detailed explanation goes here
-    getAllFiles(imagesPath)
-
+    allFiles = getAllFiles(imagesPath);
+    onlyImagesFiles = cellfun(@(x) isempty(strfind(lower(x), lower('\Images\'))) == 0, allFiles);
+    onlyImagesFiles = allFiles(onlyImagesFiles);
+    
+    onlyImageFilesNoMasks = cellfun(@(x) isempty(strfind(lower(x), 'mask')) | isempty(strfind(lower(x), 'neg')), onlyImageFiles);
+    
+    
+    %Filtering by markers
+    filterOfMarkers = zeros(size(onlyImagesFiles, 1), size(possibleMarkers, 1));
+    for numMarker = 1:size(possibleMarkers, 1)
+        filterOfMarkers(:, numMarker) = cellfun(@(x) isempty(strfind(lower(x), lower(possibleMarkers{numMarker}))) == 0, onlyImagesFiles);
+    end
 end
 
