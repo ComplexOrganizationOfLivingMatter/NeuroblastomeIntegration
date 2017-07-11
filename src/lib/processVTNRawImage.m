e3b089bd0eb5361e6ae678eb6b7a2a493ef4e804
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-11 15:10:04 +0200

    we process the raw image of VTN
    Obtaining: moderate vtn, intense vtn, cells (or green zones), blue
    zones, the white spaces and the zones that are none of the above

--- src/lib/processVTNRawImage.m
+++ src/lib/processVTNRawImage.m
@@ -1,4 +1,4 @@
-function [  ] = processVTNRawImage( image )
+function [ imgSegmented, imgOnlyWhite, imgNoMarkersNoWhite ] = processVTNRawImage( initialImage )
 %PROCESSVTNRAWIMAGE Summary of this function goes here
 %   Detailed explanation goes here
     vtnModerate = [128 0 0];
@@ -10,9 +10,21 @@ function [  ] = processVTNRawImage( image )
     
     %Seems to be fibre. We want to capture all the blue region within the
     %VTN images.
-    blueZoneRangeInit = [30 70 215];
-    blueZoneRangeEnd = [150 200 255];
+    blueZoneRange = [30 70 230; 180 200 255];
     
-
+    coloursOfSegmentations = {vtnModerate; vtnIntense; vtnLow};
+    
+    imgSegmented = cell(size(coloursOfSegmentations, 1), 1);
+    for numSegmentation = 1:size(coloursOfSegmentations, 1)
+        typeOfSegmentation = coloursOfSegmentations{numSegmentation};
+        imgSegmented(numSegmentation) = {initialImage(:, :, 1) == typeOfSegmentation(1) & initialImage(:, :, 2) == typeOfSegmentation(2) & initialImage(:, :, 3) == typeOfSegmentation(3)};
+    end
+    
+    imgBlueZoneInit = initialImage(:, :, 1) > blueZoneRange(1, 1) & initialImage(:, :, 2) > blueZoneRange(1, 2) & initialImage(:, :, 3) > blueZoneRange(1, 3);
+    imgBlueZoneEnd = initialImage(:, :, 1) < blueZoneRange(2, 1) & initialImage(:, :, 2) < blueZoneRange(2, 2) & initialImage(:, :, 3) < blueZoneRange(2, 3);
+    imgSegmented(end+1) = {imgBlueZoneInit & imgBlueZoneEnd};
+    
+    imgOnlyWhite = initialImage(:, :, 1) == 255 & initialImage(:, :, 2) == 255 & initialImage(:, :, 3) == 255;
+    imgNoMarkersNoWhite = ~imgOnlyWhite & ~imgSegmented{1} & ~imgSegmented{2} & ~imgSegmented{3} & ~imgSegmented{4};
 end
 
