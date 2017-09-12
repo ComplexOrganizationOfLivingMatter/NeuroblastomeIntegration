8ef44cd2e38bed93812034448f7bca21e1c02b14
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-12 11:32:16 +0200

    refactor

--- src/analyzeGraphletsDistances.m
+++ src/analyzeGraphletsDistances.m
@@ -3,14 +3,7 @@ function [ ] = analyzeGraphletsDistances(currentPath, marker, typeOfDistance)
 %   Detailed explanation goes here
     graphletFiles = getAllFiles(currentPath);
 
-    pacientArray = {};
-    sortingWTMeanArray = {};
-    sortingWTSizeArray = {};
-    differenceGraphletsSortingArray = {};
-    iterationWTMeanArray = {};
-    iterationWTSizeArray = {};
-    differenceGraphletsIterationArray = {};
-    mstWTMeanArray = {};
+    allCharacteristics = [];
     
     for numFile = 1:size(graphletFiles,1)
         
@@ -19,123 +12,41 @@ function [ ] = analyzeGraphletsDistances(currentPath, marker, typeOfDistance)
         graphletName = graphletNameSplitted(end);
         graphletName = graphletName{1};
 
-        if size(strfind(graphletName, typeOfDistance), 1) > 0
+        if isempty(strfind(graphletName, typeOfDistance)) == 0
+            distanceMatrix = dlmread(fullPathGraphlet, '\t', 1, 1);
+            names = importfileNames(fullPathGraphlet);
+            namesSplitted = cellfun(@(x) strsplit(x, '/'), names, 'UniformOutput', false);
+            namesFinal = cellfun(@(x) x(end), namesSplitted);
             
-            if isempty(strfind(graphletNameSplitted{end-2} , 'Control')) == 0
-                    graphletNameSplitted(end-1)
-                    distanceMatrix = dlmread(fullPathGraphlet, '\t', 1, 1);
-                    names = importfileNames(fullPathGraphlet);
-                    names = cellfun(@(x) strsplit(x, '/'), names, 'UniformOutput', false);
-                    names = cellfun(@(x) x{end}, names, 'UniformOutput', false);
-                    markerFilter = cellfun(@(x) size(strfind(lower(x), lower(marker)), 1) > 0, names);
-                    names = names(markerFilter);
-                    distanceMatrix = distanceMatrix(markerFilter, markerFilter);
-                    algorithmsFilter = cellfun(@(x) size(strfind(x, 'BetweenPairs'), 1) > 0, names);
-
-                    sortingFilter = cellfun(@(x) size(strfind(x, 'sorting'), 1) > 0, names);
-                    sortingAlgorithm = distanceMatrix(sortingFilter == 1, sortingFilter == 1);
-                    sortingNames = names(sortingFilter == 1);
-                    sortingControlFilter = cellfun(@(x) size(strfind(x, 'Control'), 1) > 0, sortingNames);
-                    sortingWTNames = sortingNames(sortingControlFilter == 0);
-                    sortingWT = sortingAlgorithm(sortingControlFilter == 0, sortingControlFilter);
-                    sortingWTMean = mean(sortingWT, 2)';
-                    if isempty(sortingWTMean)
-                        sortingWTMean = 0;
-                    end
-    %                 differenceGraphletsSorting = zeros(size(sortingWTMean, 2) - 1, 1);
-    % 
-    %                 for i = 1:size(differenceGraphletsSorting, 1)
-    %                     differenceGraphletsSorting(i) = sortingWTMean(i + 1) - sortingWTMean(i);
-    %                 end
-
-                    iterationAlgorithm = distanceMatrix(algorithmsFilter == 1, algorithmsFilter == 1);
-                    iterationNames = names(algorithmsFilter);
-                    iterationControlFilter = cellfun(@(x) size(strfind(x, 'Control'), 1) > 0, iterationNames);
-                    iterationWTNames = iterationNames(iterationControlFilter == 0);
-                    %iterationWTNamesSplitted = cellfun(@(x) strsplit(x, 'It'), iterationWTNames, 'UniformOutput', false);
-    %                 [~, it] = sort(cellfun(@(x) str2num(x{end}), iterationWTNamesSplitted));
-    %                 iterationWTNames = iterationWTNames(it);
-                    iterationWT = iterationAlgorithm(iterationControlFilter == 0, iterationControlFilter);
-
-                    %iterationWTMean = mean(iterationWT(it, :), 2)';
-                    iterationWTMean = mean(iterationWT(:, :), 2)';
-    %                 differenceGraphletsIteration = zeros(size(iterationWTMean, 2) - 1, 1);
-    % 
-    %                 for i = 1:size(differenceGraphletsIteration, 1)
-    %                     differenceGraphletsIteration(i) = iterationWTMean(i + 1) - iterationWTMean(i);
-    %                 end
-                    mstNames = names(algorithmsFilter == 0 & sortingFilter == 0);
-                    mstAlgorithm = distanceMatrix(algorithmsFilter == 0 & sortingFilter == 0, algorithmsFilter);
-                    mstWT = mstAlgorithm(:, iterationControlFilter);
-                    mstWTMean = mean(mstWT(:, :), 2)';
-                    if isempty(mstWTMean)
-                        mstWTMean = 0;
-                    end
-                    
-                    if isempty(iterationWTMean)
-                        iterationWTMean = 0;
-                    end
-                    outputFile = strjoin(graphletNameSplitted(1:end-1), '\');
-                    save(strcat(outputFile, '\meanDistanceWithControl', upper(typeOfDistance) ,'.mat'), 'sortingWTNames', 'sortingWTMean', 'iterationWTNames', 'iterationWTMean', 'mstNames', 'mstWTMean');
-            else
-                graphletNameSplitted(end-1)
-                distanceMatrixInit = dlmread(fullPathGraphlet, '\t', 1, 1);
-                names = importfileNames(fullPathGraphlet);
-                [names, it] = sort(names);
-                distanceMatrix = distanceMatrixInit(it, it);
-                pacientArray(end+1, 1) = graphletNameSplitted(end-1);
+            originalImagesFilter = cellfun(@(x) isempty(strfind(lower(x), 'maskcontigous')) == 0, namesFinal);
+            originalImagesIndices = find(originalImagesFilter);
+            
+            for numOriginalImg = 1:size(originalImagesIndices, 1)
+                actualIndex = originalImagesIndices(numOriginalImg);
+                actualOriginalImg = namesFinal{actualIndex};
                 
-                algorithmsFilter = cellfun(@(x) isempty(strfind(x, 'BetweenPairs')) == 0, names);
-                sortingWTMean = distanceMatrix(1, algorithmsFilter == 0);
-                sortingWTMean = sortingWTMean(2:end);
-                
-                differenceGraphletsSorting = zeros(size(sortingWTMean, 2) - 1, 1);
-
-                for i = 1:size(differenceGraphletsSorting, 1)
-                    differenceGraphletsSorting(i) = sortingWTMean(i + 1) - sortingWTMean(i);
+                if lower(actualOriginalImg(1)) == 'm' % mst or iteration
+                    controlIndices = find(cellfun(@(x) isempty(strfind(lower(x), 'maskcontigous')) & x(1) == 'm', namesFinal));
+                    if strcmpi(actualOriginalImg(1:3), 'mst') % Minimum Spanning Tree
+                        mstValue = mean(distanceMatrix(actualIndex, controlIndices));
+                    else % Iteration
+                        iterationValue = mean(distanceMatrix(actualIndex, controlIndices));
+                    end
+                else %sorting
+                    controlIndices = find(cellfun(@(x) isempty(strfind(lower(x), 'maskcontigous')) & x(1) == 's', namesFinal));
+                    sortingValue = mean(distanceMatrix(actualIndex, controlIndices));
                 end
-                
-                iterationWTNames = names(algorithmsFilter);
-                iterationWTNamesSplitted = cellfun(@(x) strsplit(x, 'It'), iterationWTNames, 'UniformOutput', false);
-                [~, it] = sort(cellfun(@(x) str2num(x{end}), iterationWTNamesSplitted));
-                iterationWTNames = iterationWTNames(it);
-                iterationWTMean = distanceMatrix(1, algorithmsFilter);
-                iterationWTMean = iterationWTMean(it);
-%                 differenceGraphletsIteration = zeros(size(iterationWTMean, 2) - 1, 1);
-% 
-%                 for i = 1:size(differenceGraphletsIteration, 1)
-%                     differenceGraphletsIteration(i) = iterationWTMean(i + 1) - iterationWTMean(i);
-%                 end
             end
-            
-            pacientArray(end+1, 1) = graphletNameSplitted(end-1);
-            mstWTMeanArray(end+1, 1) = {mstWTMean'};
-            sortingWTMeanArray(end+1, 1) = {sortingWTMean'};
-            %sortingWTSizeArray(end+1, 1) = {size(sortingWTMean, 2)};
-            %differenceGraphletsSortingArray(end+1, 1) = {differenceGraphletsSorting};
-            iterationWTMeanArray(end+1, 1) = {iterationWTMean'};
-            %iterationWTSizeArray(end+1, 1) = {size(iterationWTMean, 2)};
-            
-            %differenceGraphletsIterationArray(end+1, 1) = {differenceGraphletsIteration};
+            namePatient = graphletNameSplitted(end-1);
+            if isempty(allCharacteristics)
+                allCharacteristics = table(namePatient, sortingValue, iterationValue, mstValue);
+            else
+                allCharacteristics = [allCharacteristics; table(namePatient, sortingValue, iterationValue, mstValue)];
+            end
         end
     end
-    
-    pacientArray;
-    sortingWTMeanArray = padcat(sortingWTMeanArray{:});
-    %differenceGraphletsSortingArray = padcat(differenceGraphletsSortingArray{:});
-    %sortingWTSizeArray = cell2mat(sortingWTSizeArray);
-    sortingCharacteristics = [sortingWTMeanArray];
-    
-    iterationWTMeanArray = padcat(iterationWTMeanArray{:});
-    %differenceGraphletsIterationArray = padcat(differenceGraphletsIterationArray{:});
-    %iterationWTSizeArray = cell2mat(iterationWTSizeArray);
-    iterationCharacteristics = [iterationWTMeanArray];
-    mstWTMeanArray = cell2mat(mstWTMeanArray);
-    mstCharacteristics = [mstWTMeanArray];
 
-    allCharacteristics = [sortingCharacteristics, iterationCharacteristics, mstCharacteristics];
-    
     outputFile = strjoin(graphletNameSplitted(1:end-2), '\');
-    csvwrite(strcat(outputFile, '\characteristics_', marker, '_', upper(typeOfDistance), '.csv'), allCharacteristics);
+    writetable(allCharacteristics, strcat(outputFile, '\characteristics_', marker, '_', upper(typeOfDistance),'_', date ,'.xls'));
 end
 
