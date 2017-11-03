15bb7a2e86a155771fb773c62756e7c9038daee6
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-11-03 09:41:07 +0100

    catch fixing the problem and bugfix

--- src/lib/getCoupledRegions.m
+++ src/lib/getCoupledRegions.m
@@ -56,61 +56,67 @@ function [ finalHoles ] = getCoupledRegions( holes, maskFiles, radiusOfTheAreaTa
             yoffSet = ypeak-size(basicImage,1);
             xoffSet = xpeak-size(basicImage,2);
             correspondenceOfTheOldImage = [xoffSet+1, yoffSet+1, size(basicImage,2), size(basicImage,1)];
-
-            %New Image general for all markers
-            finalHoles(numHole, 4) = {imcrop(actualImg, correspondenceOfTheOldImage)};
-
-            newCentroid = regionprops(finalHoles{numHole, 4});
-            if size(newCentroid, 1) > 1
-                newCentroid = [];
-                newCentroid.Centroid = [ypeak / 2, ypeak / 2];
-            end
-            %New centroids for all markers
-            finalHoles(numHole, 5) = {[newCentroid.Centroid(1) + finalHoles{numHole, 3}.BoundingBox(1) + xoffSet, newCentroid.Centroid(2) + finalHoles{numHole, 3}.BoundingBox(2) + yoffSet]};
-
-            markerIndex = cellfun(@(x) isempty(strfind(lower(x), lower(finalHoles{numHole, 1}))) == 0, maskFiles);
-            if sum(markerIndex) > 1
-                markerIndex = cellfun(@(x) isempty(strfind(lower(x), lower(finalHoles{numHole, 1}))) == 0 & isempty(strfind(lower(x), lower(finalHoles{numHole, 12}))) == 0 , maskFiles);
-            end
-
-            img = imread(maskFiles{markerIndex});
-            img = img(:, :, 1);
-            imgWithCentroid = zeros(size(img));
-            imgWithCentroid(round(finalHoles{numHole, 5}(2)), round(finalHoles{numHole, 5}(1))) = 1;
-            imgDistance = bwdist(imgWithCentroid);
-            imgDistance = imgDistance <= radiusOfTheAreaTaken;
-            imgOfRegion = (double(img)/255) .* imgDistance;
-
-            % IMPORTANT: GET INFO OF BIOPSY TAKING INTO ACCOUNT THE HOLES.
-            % Third, we are going to get the region within the real
-            % image corresponding to the holes.
-            % There may be areas with big holes and it really couldn't contain
-            % any fibre. Therefore, we should ponderate in someway whether or
-            % not we could find much fibre within the are.
-
-            %Col 6: imgWhereFibreCanFall
-            [in, index] = ismember(finalHoles(numHole, 1), maskOfImagesByCase(:, 3));
-            if any(in)
-                finalHoles(numHole, 6) = {imresize(maskOfImagesByCase{index, 1}, size(imgDistance), 'nearest')};
-            end
-
-            %Col 7: img of region
-            finalHoles(numHole, 7) = {imgOfRegion};
-
-    %         figure; imshow(insertShape(img, 'circle', [finalHoles{numHole, 5}(1), finalHoles{numHole, 5}(2) radiusOfTheAreaTaken], 'LineWidth', 5));
-    %         close
-    %         figure; imshow(imgOfRegion);
-    %         close
-
-            finalHoles(numHole, 8) = {double(finalHoles{numHole, 6}) .* imgDistance};
-
-            finalHoles(numHole, 9) = {sum(sum(finalHoles{numHole, 7}))};
-            finalHoles(numHole, 10) = {sum(sum(finalHoles{numHole, 8}))};
-            finalHoles(numHole, 11) = {finalHoles{numHole, 9} / finalHoles{numHole, 10}};
         catch ex
             disp('Error');
             disp(ex.message)
+            correspondenceOfTheOldImage = [1, 1, size(actualImg,2), size(actualImg,1)];
+            ypeak = size(actualImg, 1);
+            xpeak = size(actualImg, 2);
+            yoffSet = 1;
+            xoffSet = 1;
         end
+        
+        %New Image general for all markers
+        finalHoles(numHole, 4) = {imcrop(actualImg, correspondenceOfTheOldImage)};
+        
+        newCentroid = regionprops(finalHoles{numHole, 4});
+        if size(newCentroid, 1) > 1
+            newCentroid = [];
+            newCentroid.Centroid = [ypeak / 2, xpeak / 2];
+        end
+        %New centroids for all markers
+        finalHoles(numHole, 5) = {[newCentroid.Centroid(1) + finalHoles{numHole, 3}.BoundingBox(1) + xoffSet, newCentroid.Centroid(2) + finalHoles{numHole, 3}.BoundingBox(2) + yoffSet]};
+        
+        markerIndex = cellfun(@(x) isempty(strfind(lower(x), lower(finalHoles{numHole, 1}))) == 0, maskFiles);
+        if sum(markerIndex) > 1
+            markerIndex = cellfun(@(x) isempty(strfind(lower(x), lower(finalHoles{numHole, 1}))) == 0 & isempty(strfind(lower(x), lower(finalHoles{numHole, 12}))) == 0 , maskFiles);
+        end
+        
+        img = imread(maskFiles{markerIndex});
+        img = img(:, :, 1);
+        imgWithCentroid = zeros(size(img));
+        imgWithCentroid(round(finalHoles{numHole, 5}(2)), round(finalHoles{numHole, 5}(1))) = 1;
+        imgDistance = bwdist(imgWithCentroid);
+        imgDistance = imgDistance <= radiusOfTheAreaTaken;
+        imgOfRegion = (double(img)/255) .* imgDistance;
+        
+        % IMPORTANT: GET INFO OF BIOPSY TAKING INTO ACCOUNT THE HOLES.
+        % Third, we are going to get the region within the real
+        % image corresponding to the holes.
+        % There may be areas with big holes and it really couldn't contain
+        % any fibre. Therefore, we should ponderate in someway whether or
+        % not we could find much fibre within the are.
+        
+        %Col 6: imgWhereFibreCanFall
+        [in, index] = ismember(finalHoles(numHole, 1), maskOfImagesByCase(:, 3));
+        if any(in)
+            finalHoles(numHole, 6) = {imresize(maskOfImagesByCase{index, 1}, size(imgDistance), 'nearest')};
+        end
+        
+        %Col 7: img of region
+        finalHoles(numHole, 7) = {imgOfRegion};
+        
+        %         figure; imshow(insertShape(img, 'circle', [finalHoles{numHole, 5}(1), finalHoles{numHole, 5}(2) radiusOfTheAreaTaken], 'LineWidth', 5));
+        %         close
+        %         figure; imshow(imgOfRegion);
+        %         close
+        
+        finalHoles(numHole, 8) = {double(finalHoles{numHole, 6}) .* imgDistance};
+        
+        finalHoles(numHole, 9) = {sum(sum(finalHoles{numHole, 7}))};
+        finalHoles(numHole, 10) = {sum(sum(finalHoles{numHole, 8}))};
+        finalHoles(numHole, 11) = {finalHoles{numHole, 9} / finalHoles{numHole, 10}};
+        
     end
     
     finalHoles = cell2table(finalHoles);
