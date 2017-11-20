3ea5927c9d1ecd75880e1030ef1750ab0c6b18c3
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-11-20 12:41:21 +0100

    returning a logical and img of distance

--- src/lib/regionOfImage.m
+++ src/lib/regionOfImage.m
@@ -1,4 +1,4 @@
-function [ imgOfRegion ] = regionOfImage( img, radiusOfTheAreaTaken, xCentroid, yCentroid )
+function [ imgOfRegion, imgDistance ] = regionOfImage( img, radiusOfTheAreaTaken, xCentroid, yCentroid )
 %REGIONOFIMAGE Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,8 +6,8 @@ imgWithCentroid = zeros(size(img));
 imgWithCentroid(round(yCentroid), round(xCentroid)) = 1;
 imgDistance = bwdist(imgWithCentroid);
 imgDistance = imgDistance <= radiusOfTheAreaTaken;
-%         figure; imshow(img); hold on; plot(round(finalHoles{numHole, 5}(2)), round(finalHoles{numHole, 5}(1)), 'r*')
-imgOfRegion = (double(img)/255) .* imgDistance;
+% figure; imshow(img); hold on; plot(round(xCentroid), round(yCentroid), 'r*')
+imgOfRegion = ((double(img)/255) .* imgDistance) > 0;
 
 end
 
