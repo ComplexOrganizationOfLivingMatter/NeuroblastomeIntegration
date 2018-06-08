2cc9b74e102f40791e579007d6631353343df642
Pablo Vicente Munuera <pvicente1@us.es>
2018-06-08 17:58:19 +0200

    table with results

--- pipelineCouplingVersion2.m
+++ pipelineCouplingVersion2.m
@@ -2,17 +2,21 @@
 cd163Files = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\CD163\Images\**\*.tif');
 retFiles = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\RET\Images\**\*.tif');
 
+results = [];
+caseNames = {};
 
 for numCD163File = 1:length(cd163Files)
     cd163ActualFile = cd163Files(numCD163File);
     if contains(cd163ActualFile.name, 'POSITIVAS') % Only positive images
         fileName = strsplit(cd163ActualFile.name, '_');
         caseName = fileName{1};
-        actualRETFile = cellfun(@(x) contains(x, caseName) & contains(x, 'CELS') == 0, {retFiles.name});
+        actualRETFile = cellfun(@(x) contains(x, caseName) & contains(x, 'CELS', 'IgnoreCase',true) == 0, {retFiles.name});
         
         if sum(actualRETFile) > 0
+            caseNames(end+1) = {caseName};
             actualRETFileName = retFiles(actualRETFile);
             
+            actualRETFileName.folder
             retImg = imread(strcat(actualRETFileName.folder, '\', actualRETFileName.name));
             cd163Img = imread(strcat(cd163ActualFile.folder, '\', cd163ActualFile.name));
             
@@ -28,27 +32,34 @@ for numCD163File = 1:length(cd163Files)
             reticulineBranchPoints = bwmorph(retSkeletonImg, 'branchpoints');
             
             [x, y] = find(reticulineBranchPoints);
-            retBranchesPoints = horzcat(y ,x);
+            retBranchesPoints = horzcat(y, x);
             
             distancesCD163_Ret = pdist2(cd163Centroids, retBranchesPoints);
             
             [minDistancesCD163_Ret, minDistancesCD163_RetPositions] = min(distancesCD163_Ret, [], 2);
             
-            %Paint closest distances
-            figure;
-            imshow(cd163Img)
-            hold on;
-            imshow(reticulineBranchPoints, colormap('parula'));
-            for numCentroidCD163 = 1:size(cd163Centroids, 1)
-                retMinPos = minDistancesCD163_RetPositions(numCentroidCD163);
-                plot([cd163Centroids(numCentroidCD163, 1) ; reticulineBranchPoints(retMinPos, 1)], [cd163Centroids(numCentroidCD163, 2); reticulineBranchPoints(retMinPos, 2)]);
-            end
+%             %Paint closest distances
+%             figure;
+%             composedImg = double(cd163Img)*40;
+%             imshow(composedImg, colormap('hot'))
+%             hold on;
+%             for numCentroidCD163 = 1:size(cd163Centroids, 1)
+%                 plot(retBranchesPoints(retMinPos, 1), retBranchesPoints(retMinPos, 2), 'r*');
+%                 retMinPos = minDistancesCD163_RetPositions(numCentroidCD163);
+%                 plot([cd163Centroids(numCentroidCD163, 1) , retBranchesPoints(retMinPos, 1)], [cd163Centroids(numCentroidCD163, 2), retBranchesPoints(retMinPos, 2)]);
+%             end
             
+            results(end+1, 1:2) = horzcat(mean(minDistancesCD163_Ret),std(minDistancesCD163_Ret));
             
             disp('');
             
         else
-            warning(strcat('No positive marker for RET ', caseName));
+            warning(strcat('No positive marker for RET: ', caseName));
         end
     end
 end
+
+resultsTable = table(caseNames', results(:, 1), results(:, 2));
+resultsTable.Properties.VariableNames{1} = 'Case';
+resultsTable.Properties.VariableNames{2} = 'MeanBranches';
+resultsTable.Properties.VariableNames{3} = 'StdBranches';
