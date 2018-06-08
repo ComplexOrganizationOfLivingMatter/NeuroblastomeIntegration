525ac202c5601382d8afef7ea96b9a95c5c3b1a1
Pablo Vicente Munuera <pvicente1@us.es>
2018-06-08 15:41:08 +0200

    init coupling markers

---
+++ pipelineCouplingVersion2.m
@@ -0,0 +1,3 @@
+
+cd163Files = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\CD163\Images\**\*');
+retFiles = '
---
+++ src/couplingCD163andRET/couplingMarkers.m
@@ -0,0 +1,7 @@
+function [] = couplingMarkers()
+%COUPLINGMARKERS Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+end
