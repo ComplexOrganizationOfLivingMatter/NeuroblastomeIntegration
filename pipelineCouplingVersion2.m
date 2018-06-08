79b0eee2f5749adf393b50e1d3e4e91dff9bca1b
Pablo Vicente Munuera <pvicente1@us.es>
2018-06-08 15:59:22 +0200

    getting both images rET and CD163

--- pipelineCouplingVersion2.m
+++ pipelineCouplingVersion2.m
@@ -1,3 +1,20 @@
 
-cd163Files = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\CD163\Images\**\*');
-retFiles = '
+cd163Files = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\CD163\Images\**\*.tif');
+retFiles = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\RET\Images\**\*.tif');
+
+
+for numCD163File = 1:length(cd163Files)
+    cd163ActualFile = cd163Files(numCD163File);
+    if contains(cd163ActualFile.name, 'POSITIVAS') % Only positive images
+        fileName = strsplit(cd163ActualFile.name, '_');
+        caseName = fileName{1};
+        actualRETFile = cellfun(@(x) contains(x, caseName) & contains(x, 'CELS') == 0, {retFiles.name});
+        
+        if sum(actualRETFile) > 0
+            actualRETFileName = retFiles(actualRETFile);
+            
+        else
+            warning(strcat('No positive marker for RET ', caseName));
+        end
+    end
+end
