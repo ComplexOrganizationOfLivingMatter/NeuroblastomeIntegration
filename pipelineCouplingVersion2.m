24ce082f1d69031edad69f8b2e67bae517f0dea3
Pablo Vicente Munuera <pvicente1@us.es>
2018-06-08 16:28:07 +0200

    getting centroids of binary image 1

--- pipelineCouplingVersion2.m
+++ pipelineCouplingVersion2.m
@@ -13,6 +13,17 @@ for numCD163File = 1:length(cd163Files)
         if sum(actualRETFile) > 0
             actualRETFileName = retFiles(actualRETFile);
             
+            retImg = imread(strcat(actualRETFileName.folder, '\', actualRETFileName.name));
+            cd163Img = imread(strcat(cd163ActualFile.folder, '\', cd163ActualFile.name));
+            
+            retImg = retImg(:, :, 1);
+            cd163Img = cd163Img(:, :, 1);
+            
+            cd163Centroids = regionprops(cd163Img > 0, 'Centroid');
+            cd163Centroids = vertcat(cd163Centroids.Centroid);
+            
+            disp('');
+            
         else
             warning(strcat('No positive marker for RET ', caseName));
         end
