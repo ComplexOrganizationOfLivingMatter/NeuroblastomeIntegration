14003f012182417fd4e7f98e4d7fb06719792384
Pablo Vicente Munuera <pvicente1@us.es>
2018-06-11 11:21:25 +0200

    correspondance of risk with cases

--- pipelineCouplingVersion2.m
+++ pipelineCouplingVersion2.m
@@ -2,6 +2,99 @@
 cd163Files = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\CD163\Images\**\*.tif');
 retFiles = dir('D:\Pablo\Neuroblastoma\Datos\Data\NuevosCasos160\Casos\RET\Images\**\*.tif');
 
+casesRisk = {'00B18084'	'High'	'HighRisk'
+'00B18416'	'High'	'HighRisk'
+'01B21578'	'High'	'HighRisk'
+'02B08103'	'High'	'HighRisk'
+'02B20617'	'High'	'HighRisk'
+'04B00051'	''	'NoRisk'
+'04B00356'	'Medium'	'NoRisk'
+'04B01940'	''	'NoRisk'
+'04B03320'	''	'NoRisk'
+'04B03964'	'Low'	'NoRisk'
+'04B04150'	'VeryLow'	'NoRisk'
+'04B05490'	'VeryLow'	'NoRisk'
+'04B07005'	'Low'	'NoRisk'
+'04B07651'	'VeryLow'	'NoRisk'
+'04B08057'	'High'	'HighRisk'
+'04B09873'	'VeryLow'	'NoRisk'
+'04B10379'	'Low'	'NoRisk'
+'04B11947'	'High'	'NoRisk'
+'04B00383'	'VeryLow'	'NoRisk'
+'05B00556'	'VeryLow'	'NoRisk'
+'05B00825'	'Low'	'NoRisk'
+'05B00835'	'VeryLow'	'NoRisk'
+'05B03756'	'Low'	'NoRisk'
+'05B03946'	''	'NoRisk'
+'05B05867'	'VeryLow'	'NoRisk'
+'05B06560'	'Low'	'NoRisk'
+'05B08443'	''	'NoRisk'
+'05B09222'	'Low'	'NoRisk'
+'05B12938'	'VeryLow'	'NoRisk'
+'05B14881'	'Low'	'NoRisk'
+'05B16927'	'Medium'	'HighRisk'
+'05B17767'	''	'NoRisk'
+'05B24845'	'Medium'	'HighRisk'
+'06B00069'	'Medium'	'NoRisk'
+'06B00134'	'Low'	'NoRisk'
+'06B01443'	'Medium'	'HighRisk'
+'06B01779'	'Low'	'NoRisk'
+'06B05331'	'VeryLow'	'NoRisk'
+'06B05411'	'VeryLow'	'NoRisk'
+'06B05418'	'Medium'	'HighRisk'
+'06B06380'	'Medium'	'HighRisk'
+'06B06382'	'Low'	'NoRisk'
+'06B06383'	'Medium'	'HighRisk'
+'06B06639'	'VeryLow'	'NoRisk'
+'06B07014'	'Medium'	'HighRisk'
+'06B10032'	'VeryLow'	'NoRisk'
+'06B12728'	'High'	'HighRisk'
+'06B12729'	'High'	'HighRisk'
+'06B19686'	'Low'	'NoRisk'
+'06B20558'	'VeryLow'	'NoRisk'
+'07B00135'	'VeryLow'	'NoRisk'
+'07B00317'	'Medium'	'HighRisk'
+'07B01279'	'Medium'	'HighRisk'
+'07B02578'	'VeryLow'	'NoRisk'
+'07B02644'	'High'	'HighRisk'
+'07B03435'	'Low'	'NoRisk'
+'07B04839'	'VeryLow'	'NoRisk'
+'07B05377'	'Low'	'NoRisk'
+'07B05473'	'VeryLow'	'NoRisk'
+'07B06459'	'VeryLow'	'NoRisk'
+'07B07027'	'High'	'HighRisk'
+'07B07615'	''	'NoRisk'
+'07B08344'	''	'NoRisk'
+'07B10124'	'VeryLow'	'NoRisk'
+'07B10505'	'High'	'HighRisk'
+'07B11108'	'Low'	'NoRisk'
+'07B12190'	'Medium'	'NoRisk'
+'07B12410'	'Low'	'NoRisk'
+'07B12522'	'VeryLow'	'NoRisk'
+'07B13306'	'VeryLow'	'NoRisk'
+'07B14162'	'Low'	'NoRisk'
+'07B14193'	''	'NoRisk'
+'07B14628'	'Medium'	'NoRisk'
+'07B17220'	'High'	'HighRisk'
+'07B17225'	'Medium'	'NoRisk'
+'07B17284'	'VeryLow'	'NoRisk'
+'07B17311'	'VeryLow'	'NoRisk'
+'07B17329'	'Low'	'NoRisk'
+'07B19261'	'VeryLow'	'NoRisk'
+'07B19275'	'Low'	'NoRisk'
+'07B19436'	'Low'	'NoRisk'
+'10B30220'	'High'	'NoRisk'
+'11B07120'	'High'	'HighRisk'
+'11B14142'	'High'	'HighRisk'
+'11B17262'	'High'	'HighRisk'
+'12B13476'	'High'	'HighRisk'
+'13B29977'	'High'	'NoRisk'
+'15B09301'	'High'	'NoRisk'
+'15B23112'	'High'	'HighRisk'
+'306707'	'VeryLow'	'NoRisk'
+'332948'	'High'	'HighRisk'
+};
+
 results = [];
 caseNames = {};
 
@@ -55,6 +148,13 @@ for numCD163File = 1:length(cd163Files)
             
         else
             warning(strcat('No positive marker for RET: ', caseName));
+            fid = fopen('logFile','a+');
+            % write the error to file
+            % first line: message
+            fprintf(fid,'%s\r\n', caseName);
+            
+            % close file
+            fclose(fid);
         end
     end
 end
@@ -63,3 +163,15 @@ resultsTable = table(caseNames', results(:, 1), results(:, 2));
 resultsTable.Properties.VariableNames{1} = 'Case';
 resultsTable.Properties.VariableNames{2} = 'MeanBranches';
 resultsTable.Properties.VariableNames{3} = 'StdBranches';
+
+correspondance = cellfun(@(x) find(contains(resultsTable{:, 1}, x)) , casesRisk(:, 1), 'UniformOutput', false);
+
+casesRisk( cellfun(@(x) isempty(x), correspondance), :) = [];
+correspondance( cellfun(@(x) isempty(x), correspondance), :) = [];
+correspondance = cell2mat(correspondance);
+
+resultsTable(:, end+1:end+2) = casesRisk(correspondance, 2:3);
+resultsTable.Properties.VariableNames{4} = 'Instability';
+resultsTable.Properties.VariableNames{5} = 'RiskCalculated';
+resultsTable = movevars(resultsTable, 'Instability', 'Before', 'MeanBranches');
+resultsTable = movevars(resultsTable, 'RiskCalculated', 'Before', 'MeanBranches');
