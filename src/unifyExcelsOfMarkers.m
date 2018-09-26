f76281710425cec5a951a3df682b543026406421
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-26 10:31:08 +0200

    unifying excels markers

---
+++ src/unifyExcelsOfMarkers.m
@@ -0,0 +1,8 @@
+function [] = unifyExcelsOfMarkers(directoryNames)
+%UNIFYEXCELSOFMARKERS Summary of this function goes here
+%   Detailed explanation goes here
+
+    for numDirectory = 1:size(directoryNames, 1)
+        
+    end
+end
