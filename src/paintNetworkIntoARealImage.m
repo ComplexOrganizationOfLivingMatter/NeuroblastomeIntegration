bbf971516e059bb0024069190e89a5c8ceb74c29
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-27 13:05:30 +0200

    painting network into real image

--- src/paintNetworkIntoARealImage.m
+++ src/paintNetworkIntoARealImage.m
@@ -2,19 +2,23 @@ function [ ] = paintNetworkIntoARealImage( imageName, adjacencyMatrix, centroids
 %PAINTNETWORKINTOAREALIMAGE Summary of this function goes here
 %   Detailed explanation goes here
     h = figure('visible', 'off');
-    %centroids = {centroids.Centroid};
-    %centroids = padcat(centroids{:});
+    if isfield(centroids, 'Centroid')
+        centroids = {centroids.Centroid};
+        centroids = padcat(centroids{:});
+    end
+    
     img = imread(imageName);
     %Only network
-    %img(img>0) = 0;
+    img(img>0) = 0;
     imshow(img);
     colormap hot;
     hold on;
     for i = 1:size(adjacencyMatrix, 1)
         for j = 1:size(adjacencyMatrix, 2)
             if adjacencyMatrix(i, j) > 0
-                plot([centroids(i, 1), centroids(j, 1)], [centroids(i, 2), centroids(j, 2)], 'r-');
+                plot([centroids(i, 1), centroids(j, 1)], [centroids(i, 2), centroids(j, 2)], 'w-');
                 %plot([centroids(i, 1), centroids(j, 1)], [centroids(i, 2), centroids(j, 2)], 'r.');
+                %plot([centroids(i, 1), centroids(j, 1)], [centroids(i, 2), centroids(j, 2)], 'k-');
             end
         end
     end
